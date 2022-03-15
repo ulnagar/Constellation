b89094f1a46570a6e097cc5bda93038e264231dd
Ben <ben@hillsley.dev>
2022-03-15 16:21:27 +1100

    Build logic chain for Exam Uploads page.

---
+++ src/Constellation.Application/DTOs/CanvasAssignmentDto.cs
@@ -0,0 +1,32 @@
+﻿using Constellation.Core.Models;
+using System;
+
+namespace Constellation.Application.DTOs
+{
+    public class CanvasAssignmentDto
+    {
+        public Guid Id { get; set; }
+        public int? CourseId { get; set; }
+        public string Name { get; set; }
+        public DateTime DueDate { get; set; }
+        public DateTime? LockDate { get; set; }
+        public DateTime? UnlockDate { get; set; }
+        public int AllowedAttempts { get; set; }
+
+        public static CanvasAssignmentDto ConvertFromAssignment(CanvasAssignment assignment)
+        {
+            var viewModel = new CanvasAssignmentDto
+            {
+                Id = assignment.Id,
+                CourseId = assignment.CourseId,
+                Name = assignment.Name,
+                DueDate = assignment.DueDate,
+                LockDate = assignment.LockDate,
+                UnlockDate = assignment.UnlockDate,
+                AllowedAttempts = assignment.AllowedAttempts
+            };
+
+            return viewModel;
+        }
+    }
+}
