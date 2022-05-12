3b9903b5f54a3a6f5a624e4f98d6d6821c8580e5
Ben <ben@hillsley.dev>
2022-05-12 15:40:00 +1000

    Reinstate the top level existing absence fields until the process can be fixed.

--- src/Constellation.Infrastructure/Jobs/AbsenceProcessingJob.cs
+++ src/Constellation.Infrastructure/Jobs/AbsenceProcessingJob.cs
@@ -697,9 +697,9 @@ namespace Constellation.Infrastructure.Jobs
                     if (existingAbsences.All(innerabsence => !innerabsence.Explained) && acceptedReasons.Contains(absence.AbsenceReason))
                     {
                         _logger.LogInformation("{id}: Student {student} ({grade}): Found external explaination for {Type} absence on {Date} - {PeriodName}", JobId, student.DisplayName, student.CurrentGrade.AsName(), absence.Type, absence.Date.ToShortDateString(), absence.PeriodName);
-                        //existingAbsence.ExternallyExplained = true;
-                        //existingAbsence.ExternalExplanation = absence.ExternalExplanation;
-                        //existingAbsence.ExternalExplanationSource = absence.ExternalExplanationSource;
+                        existingAbsence.ExternallyExplained = true;
+                        existingAbsence.ExternalExplanation = absence.ExternalExplanation;
+                        existingAbsence.ExternalExplanationSource = absence.ExternalExplanationSource;
 
                         var response = new AbsenceResponse
                         {
