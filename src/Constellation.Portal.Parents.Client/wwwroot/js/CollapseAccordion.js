723db9a315e91d6600be84ad73b14198fb380e7a
Ben <ben@hillsley.dev>
2022-09-20 17:00:26 +1000

    Start working on way to enforce the accordion is shut when the data changes.

---
+++ src/Constellation.Portal.Parents.Client/wwwroot/js/CollapseAccordion.js
@@ -0,0 +1,8 @@
+﻿window.collapseAccordion = () => {
+    console.log('Collapsing!');
+    //$('.accordion-collapse').collapse('hide');
+    var collapseElementList = [].slice.call(document.querySelectorAll('.collapse'));
+    var collapseList = collapseElementList.map(function (collapseEl) {
+        return new bootstrap.Collapse(collapseEl, { hide: false });
+    });
+}
