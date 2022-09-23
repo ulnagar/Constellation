5b906bb37ef34f74026371a700c11e3c91b2b759
Ben <ben@hillsley.dev>
2022-09-23 15:45:04 +1000

    Complete the awards module for the parent portal.

---
+++ src/Constellation.Portal.Parents.Server/Controllers/AwardsController.cs
@@ -0,0 +1,31 @@
+﻿namespace Constellation.Portal.Parents.Server.Controllers;
+
+using Constellation.Application.Features.Awards.Models;
+using Constellation.Application.Features.Awards.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+[Route("[controller]")]
+public class AwardsController : BaseAPIController
+{
+    private readonly ILogger<AwardsController> _logger;
+    private readonly IMediator _mediator;
+
+    public AwardsController(ILogger<AwardsController> logger, IMediator mediator)
+    {
+        _logger = logger;
+        _mediator = mediator;
+    }
+
+    [HttpGet("Student/{id}")]
+    public async Task<StudentAwardSummary> GetDetails([FromRoute] string Id)
+    {
+        var user = await GetCurrentUser();
+
+        _logger.LogInformation("Requested to retrieve award details for student {id} by parent {name}", Id, user.UserName);
+
+        var summary = await _mediator.Send(new GetAwardSummaryForStudentQuery { StudentId = Id });
+
+        return summary;
+    }
+}
