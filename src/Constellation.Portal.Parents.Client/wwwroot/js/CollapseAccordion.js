15e408b4a9000b15cdfcbb6dfecfbf2e97e2bc3b
Ben <ben@hillsley.dev>
2022-09-23 10:44:09 +1000

    Migrate js files in client project to app.js to ensure they are included in published output.

--- src/Constellation.Portal.Parents.Client/wwwroot/js/CollapseAccordion.js
+++
@@ -1,8 +0,0 @@
-﻿window.collapseAccordion = () => {
-    console.log('Collapsing!');
-    //$('.accordion-collapse').collapse('hide');
-    var collapseElementList = [].slice.call(document.querySelectorAll('.collapse'));
-    var collapseList = collapseElementList.map(function (collapseEl) {
-        return new bootstrap.Collapse(collapseEl, { hide: false });
-    });
-}
---
+++ src/Constellation.Portal.Parents.Client/wwwroot/js/app.js
@@ -0,0 +1,20 @@
+﻿window.collapseAccordion = () => {
+    console.log('Collapsing!');
+    //$('.accordion-collapse').collapse('hide');
+    var collapseElementList = [].slice.call(document.querySelectorAll('.collapse'));
+    var collapseList = collapseElementList.map(function (collapseEl) {
+        return new bootstrap.Collapse(collapseEl, { hide: false });
+    });
+}
+
+window.downloadFileFromStream = async (fileName, contentStreamReference) => {
+    const arrayBuffer = await contentStreamReference.arrayBuffer();
+    const blob = new Blob([arrayBuffer]);
+    const url = URL.createObjectURL(blob);
+    const anchorElement = document.createElement('a');
+    anchorElement.href = url;
+    anchorElement.download = fileName ?? '';
+    anchorElement.click();
+    anchorElement.remove();
+    URL.revokeObjectURL(url);
+}
