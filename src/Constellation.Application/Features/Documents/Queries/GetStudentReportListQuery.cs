b1ae5609fe0bff8b9f9f1b7ecbf79f2eb14c5a6b
Ben <ben@hillsley.dev>
2022-09-27 22:29:30 +1000

    Allow download of academic reports from parent portal. Refactor to clean up database file delivery options. Fix loader animation.

---
+++ src/Constellation.Application/Features/Documents/Queries/GetStudentReportListQuery.cs
@@ -0,0 +1,47 @@
+﻿namespace Constellation.Application.Features.Documents.Queries;
+
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Constellation.Application.Common.Mapping;
+using Constellation.Application.Interfaces.Repositories;
+using Constellation.Core.Models;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class GetStudentReportListQuery : IRequest<ICollection<StudentReportForList>>
+{
+    public string StudentId { get; set; }
+}
+
+public class StudentReportForList : IMapFrom<StudentReport>
+{
+    public string PublishId { get; set; }
+    public Guid Id { get; set; }
+    public string Year { get; set; }
+    public string ReportingPeriod { get; set; }
+}
+
+public class GetStudentReportListQueryHandler : IRequestHandler<GetStudentReportListQuery, ICollection<StudentReportForList>>
+{
+    private readonly IAppDbContext _context;
+    private readonly IMapper _mapper;
+
+    public GetStudentReportListQueryHandler(IAppDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    public async Task<ICollection<StudentReportForList>> Handle(GetStudentReportListQuery request, CancellationToken cancellationToken)
+    {
+        return await _context.StudentReports
+            .Where(report => report.StudentId == request.StudentId)
+            .ProjectTo<StudentReportForList>(_mapper.ConfigurationProvider)
+            .ToListAsync(cancellationToken);
+    }
+}
---
+++ src/Constellation.Portal.Parents.Server/Controllers/ReportsController.cs
@@ -0,0 +1,41 @@
+﻿namespace Constellation.Portal.Parents.Server.Controllers;
+
+using Constellation.Application.Features.Documents.Queries;
+using Constellation.Application.Features.Portal.School.Reports.Queries;
+using Constellation.Core.Models;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+[Route("[controller]")]
+public class ReportsController : BaseAPIController
+{
+	private readonly ILogger<ReportsController> _logger;
+	private readonly IMediator _mediator;
+
+	public ReportsController(ILogger<ReportsController> logger, IMediator mediator)
+	{
+		_logger = logger;
+		_mediator = mediator;
+	}
+
+	[HttpGet("Student/{studentId}")]
+	public async Task<ICollection<StudentReportForList>> GetAllContacts([FromRoute] string studentId)
+	{
+		var user = await GetCurrentUser();
+
+		_logger.LogInformation("Requested to retrieve contacts for student {studentId} by user {user}", studentId, user.UserName);
+
+		return await _mediator.Send(new GetStudentReportListQuery { StudentId = studentId });
+	}
+
+    [HttpPost("Download/{entryId}")]
+    public async Task<IActionResult> GetAttendanceReport([FromRoute] string entryId)
+    {
+        // Create file as stream
+        var fileEntry = await _mediator.Send(new GetFileFromDatabaseQuery { LinkType = StoredFile.StudentReport, LinkId = entryId });
+
+        var filename = $"Academic Report - {DateTime.Today:yyyy-MM-dd}.pdf";
+
+		return File(new MemoryStream(fileEntry.FileData), "application/pdf", filename);
+    }
+}
