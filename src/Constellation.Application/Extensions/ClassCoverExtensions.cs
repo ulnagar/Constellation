a294ff3dc69dd4c135887cf906b0723a425e1ea9
Ben <ben@hillsley.au>
2022-10-10 15:57:31 +1100

    Extract IsCurrent and IsFuture from ClassCover model and move to extension for testability.

--- src/Constellation.Application/Extensions/ClassCoverExtensions.cs
+++ src/Constellation.Application/Extensions/ClassCoverExtensions.cs
@@ -1,27 +1,27 @@
 ﻿namespace Constellation.Application.Extensions;
 
+using Constellation.Application.Interfaces.Providers;
 using Constellation.Core.Models;
-using System;
 
 public static class ClassCoverExtensions
 {
-    public static bool IsCurrent(this ClassCover cover)
+    public static bool IsCurrent(this ClassCover cover, IDateTimeProvider dateTimeProvider)
     {
         if (cover.IsDeleted == true)
             return false;
 
-        if (cover.EndDate <= DateTime.Today)
+        if (cover.EndDate <= dateTimeProvider.Today)
             return false;
 
-        if (cover.StartDate >= DateTime.Today)
+        if (cover.StartDate >= dateTimeProvider.Today)
             return false;
 
         return true;
     }
 
-    public static bool IsFuture(this ClassCover cover)
+    public static bool IsFuture(this ClassCover cover, IDateTimeProvider dateTimeProvider)
     {
-        if (cover.StartDate.Date > DateTime.Today && !cover.IsDeleted)
+        if (cover.StartDate.Date > dateTimeProvider.Today && !cover.IsDeleted)
             return true;
 
         return false;
--- src/Constellation.Infrastructure/Services/CasualService.cs
+++ src/Constellation.Infrastructure/Services/CasualService.cs
@@ -98,7 +98,7 @@ namespace Constellation.Infrastructure.Services
             if (casual == null)
                 return;
 
-            var outstandingCovers = await _unitOfWork.Covers.OutstandingForCasual(casualId);
+            var outstandingCovers = await _unitOfWork.Covers.OutstandingForCasual(casualId, _dateTimeProvider);
 
             // Remove all current casual covers
             foreach (var cover in outstandingCovers)
