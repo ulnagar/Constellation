79053b69e338376520922d3ddb1d201eb5a4d9f1
Ben <ben@hillsley.au>
2022-11-01 11:30:44 +1100

    Add ability to retire and reinstate a training module.

---
+++ src/Constellation.Application/Features/MandatoryTraining/Commands/RetireTrainingModuleCommand.cs
@@ -0,0 +1,39 @@
+﻿using Constellation.Application.Interfaces.Repositories;
+using Constellation.Core.Models.MandatoryTraining;
+namespace Constellation.Application.Features.MandatoryTraining.Commands;
+
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+public record RetireTrainingModuleCommand : IRequest
+{
+    public Guid Id { get; set; }
+	public string DeletedBy { get; set; }
+	public DateTime DeletedAt { get; set; }
+}
+
+public class RetireTrainingModuleCommandHandler : IRequestHandler<RetireTrainingModuleCommand>
+{
+	private readonly IAppDbContext _context;
+
+	public RetireTrainingModuleCommandHandler(IAppDbContext context)
+	{
+		_context = context;
+	}
+
+	public async Task<Unit> Handle(RetireTrainingModuleCommand request, CancellationToken cancellationToken)
+	{
+        var entity = await _context.MandatoryTraining.Modules
+            .FirstOrDefaultAsync(module => module.Id == request.Id, cancellationToken);
+
+        entity.DeletedBy = request.DeletedBy;
+		entity.DeletedAt = request.DeletedAt;
+
+		await _context.SaveChangesAsync(cancellationToken);
+
+		return Unit.Value;
+	}
+}
