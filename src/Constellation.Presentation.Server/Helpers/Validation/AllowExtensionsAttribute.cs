c17c73c4d4367388253eba691340aed52d27fdd9
Ben <ben@hillsley.dev>
2022-11-06 20:36:43 +1100

    Allow teachers to upload certificates with their training completion records.

---
+++ src/Constellation.Presentation.Server/Helpers/Validation/AllowExtensionsAttribute.cs
@@ -0,0 +1,35 @@
+﻿namespace Constellation.Presentation.Server.Helpers.Validation;
+
+using System.ComponentModel.DataAnnotations;
+
+[AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+public class AllowExtensionsAttribute : ValidationAttribute
+{
+    private List<string> AllowedExtensions { get; set; }
+
+    /// <summary>
+    /// Limit the uploaded files to one or more file extensions
+    /// </summary>
+    /// <param name="FileExtensions">
+    /// A comma separated list of file extensions to be allowed.
+    /// E.g. "pdf,xlsx"
+    /// </param>
+    public AllowExtensionsAttribute(string FileExtensions)
+    {
+        AllowedExtensions = FileExtensions.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+    }
+
+    public override bool IsValid(object value)
+    {
+        IFormFile file = value as IFormFile;
+        bool isValid = true;
+
+        if (file != null)
+        {
+            var fileName = file.FileName;
+            isValid = AllowedExtensions.Any(y => fileName.EndsWith(y));
+        }
+
+        return isValid;
+    }
+}
