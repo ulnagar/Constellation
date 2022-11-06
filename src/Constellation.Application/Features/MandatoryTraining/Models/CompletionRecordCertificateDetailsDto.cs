933643bb9dc56c8e262d3baf2c767c9de07df767
Ben <ben@hillsley.dev>
2022-11-06 21:26:26 +1100

    Allow view of uploaded certificates on details view, and confirmation of previously uploaded certificate in edit mode of upsert page.

---
+++ src/Constellation.Application/Features/MandatoryTraining/Models/CompletionRecordCertificateDetailsDto.cs
@@ -0,0 +1,20 @@
+﻿namespace Constellation.Application.Features.MandatoryTraining.Models;
+
+using AutoMapper;
+using Constellation.Application.Common.Mapping;
+using Constellation.Core.Models;
+using System;
+
+public class CompletionRecordCertificateDetailsDto : IMapFrom<StoredFile>
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string FileType { get; set; }
+    public string FileData { get; set; }
+
+    public void Mapping(Profile profile)
+    {
+        profile.CreateMap<StoredFile, CompletionRecordCertificateDetailsDto>()
+            .ForMember(dest => dest.FileData, opt => opt.MapFrom(src => Convert.ToBase64String(src.FileData)));
+    }
+}
---
+++ src/Constellation.Application/Features/MandatoryTraining/Models/CompletionRecordCertificateDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Constellation.Application.Features.MandatoryTraining.Models;
+
+using Constellation.Application.Common.Mapping;
+using Constellation.Core.Models;
+
+public class CompletionRecordCertificateDto : IMapFrom<StoredFile>
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string FileType { get; set; }
+}
