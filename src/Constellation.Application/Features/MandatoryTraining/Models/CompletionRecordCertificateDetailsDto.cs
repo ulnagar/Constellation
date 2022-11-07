60bdc8134eeb65865e1e75094b161cb695a55de5
Ben <ben@hillsley.dev>
2022-11-07 16:23:42 +1100

    Include icons in the Completion Records index page.

--- src/Constellation.Application/Features/MandatoryTraining/Models/CompletionRecordCertificateDetailsDto.cs
+++ src/Constellation.Application/Features/MandatoryTraining/Models/CompletionRecordCertificateDetailsDto.cs
@@ -10,11 +10,12 @@ public class CompletionRecordCertificateDetailsDto : IMapFrom<StoredFile>
     public int Id { get; set; }
     public string Name { get; set; }
     public string FileType { get; set; }
-    public string FileData { get; set; }
+    public byte[] FileData { get; set; }
+    public string FileDataBase64 { get; set; }
 
     public void Mapping(Profile profile)
     {
         profile.CreateMap<StoredFile, CompletionRecordCertificateDetailsDto>()
-            .ForMember(dest => dest.FileData, opt => opt.MapFrom(src => Convert.ToBase64String(src.FileData)));
+            .ForMember(dest => dest.FileDataBase64, opt => opt.MapFrom(src => Convert.ToBase64String(src.FileData)));
     }
 }
