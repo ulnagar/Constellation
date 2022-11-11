73ffa4a0deedb20a7598072c91803c895d9477dd
Ben <ben@hillsley.au>
2022-11-11 11:08:09 +1100

    Raise validation error if the completion record date is set beyond today.

---
+++ src/Constellation.Presentation.Server/Helpers/Validation/NotFutureDateAttribute.cs
@@ -0,0 +1,21 @@
+﻿namespace Constellation.Presentation.Server.Helpers.Validation;
+
+using System.ComponentModel.DataAnnotations;
+
+public class NotFutureDateAttribute : ValidationAttribute
+{
+	public string GetErrorMessage() =>
+		$"Date must not be in the future.";
+
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+		var date = (DateTime)value;
+
+		if (date > DateTime.Today)
+		{
+			return new ValidationResult(GetErrorMessage());
+		}
+
+		return ValidationResult.Success;
+    }
+}
