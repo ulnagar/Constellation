c8b3a889c7d4db664f825a3c259de36cfc936b0b
Ben <ben@hillsley.au>
2022-11-29 16:01:02 +1100

    Create proof of concept importer for training data in the Test Razor Page.

--- src/Constellation.Application/Interfaces/Services/IExcelService.cs
+++ src/Constellation.Application/Interfaces/Services/IExcelService.cs
@@ -1,5 +1,6 @@
 ﻿using Constellation.Application.DTOs;
 using Constellation.Application.Features.MandatoryTraining.Models;
+using Constellation.Core.Models.MandatoryTraining;
 using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
@@ -13,5 +14,6 @@ namespace Constellation.Application.Interfaces.Services
         Task<MemoryStream> CreateAwardsCalculationFile(MemoryStream stream);
         Task<MemoryStream> CreateTrainingModuleReportFile(ModuleDetailsDto data);
         Task<MemoryStream> CreateTrainingModuleStaffReportFile(StaffCompletionListDto data);
+        List<TrainingModule> ImportMandatoryTrainingDataFromFile(MemoryStream excelFile);
     }
 }
