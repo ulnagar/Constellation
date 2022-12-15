1dde70a0c9b5847574970bd497a34f6adbac3f76
Ben <ben@hillsley.au>
2022-12-16 09:34:57 +1100

    Build repository for Group Tutorial object.

---
+++ src/Constellation.Infrastructure/Persistence/ConstellationContext/Repositories/GroupTutorialRepository.cs
@@ -0,0 +1,35 @@
+﻿#nullable enable
+namespace Constellation.Infrastructure.Persistence.ConstellationContext.Repositories;
+
+using Constellation.Core.Abstractions;
+using Constellation.Core.Models.GroupTutorials;
+using Microsoft.EntityFrameworkCore;
+
+internal sealed class GroupTutorialRepository : IGroupTutorialRepository
+{
+    private readonly AppDbContext _dbContext;
+
+    public GroupTutorialRepository(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<GroupTutorial?> GetWholeAggregate(
+        Guid id, 
+        CancellationToken cancellationToken = default) =>
+        await _dbContext
+            .Set<GroupTutorial>()
+            .AsSplitQuery()
+            .Include(tutorial => tutorial.Enrolments)
+            .Include(tutorial => tutorial.Teachers)
+            .Include(tutorial => tutorial.Rolls)
+            .ThenInclude(roll => roll.Students)
+            .Where(tutorial => tutorial.Id == id)
+            .FirstOrDefaultAsync(cancellationToken);
+
+    public void Insert(GroupTutorial tutorial) =>
+        _dbContext.Set<GroupTutorial>().Add(tutorial);
+
+    public void Remove(GroupTutorial tutorial) =>
+        _dbContext.Set<GroupTutorial>().Remove(tutorial);
+}
