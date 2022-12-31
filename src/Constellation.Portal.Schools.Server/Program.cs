ec39f029caaaf0413d748e9b3e0813cb89f355d4
Ben <ben@hillsley.au>
2022-12-31 21:05:08 +1100

    Add the new projects for the wasm version of the schools portal.

---
+++ src/Constellation.Portal.Schools.Server/Program.cs
@@ -0,0 +1,45 @@
+using Constellation.Infrastructure.DependencyInjection;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog();
+LoggingConfiguration.SetupLogging(builder.Configuration, Serilog.Events.LogEventLevel.Debug);
+
+builder.Services.AddSchoolPortalInfrastructureComponents(builder.Configuration);
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UsePathBase("/schools");
+app.UseHttpsRedirection();
+
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseIdentityServer();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapRazorPages();
+app.MapControllers();
+app.MapFallbackToFile("index.html");
+
+app.Run();
+ 
