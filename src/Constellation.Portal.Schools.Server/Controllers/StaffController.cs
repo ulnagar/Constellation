4df15f0c632bde577893f883e29850a114a5b23b
Ben <ben@hillsley.dev>
2023-01-11 21:01:06 +1100

    Migrate stocktake and help sections.

---
+++ src/Constellation.Portal.Schools.Server/Controllers/StaffController.cs
@@ -0,0 +1,31 @@
+﻿namespace Constellation.Portal.Schools.Server.Controllers;
+
+using Constellation.Application.Features.Portal.School.Home.Models;
+using Constellation.Application.Features.Portal.School.Home.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+[Route("api/[controller]")]
+public class StaffController : BaseAPIController
+{
+    private readonly IMediator _mediator;
+    private readonly Serilog.ILogger _logger;
+
+    public StaffController(IMediator mediator, Serilog.ILogger logger)
+    {
+        _mediator = mediator;
+        _logger = logger.ForContext<StaffController>();
+    }
+
+    [HttpGet("ForDropdown/{schoolCode}")]
+    public async Task<List<StaffFromSchoolForDropdownSelection>> GetStaffForDropdown([FromRoute] string schoolCode)
+    {
+        var user = await GetCurrentUser();
+
+        _logger.Information("Requested to retrieve staff members for school {schoolCode} by user {user}", schoolCode, user.DisplayName);
+
+        var staff = await _mediator.Send(new GetStaffFromSchoolForSelectionQuery { SchoolCode = schoolCode });
+
+        return staff.ToList();
+    }
+}
--- src/Constellation.Portal.Schools.Server/Controllers/StocktakeController.cs
+++ src/Constellation.Portal.Schools.Server/Controllers/StocktakeController.cs
@@ -1,8 +1,13 @@
 ﻿namespace Constellation.Portal.Schools.Server.Controllers;
 
 using Constellation.Application.Features.Portal.School.Stocktake.Commands;
+using Constellation.Application.Features.Portal.School.Stocktake.Models;
+using Constellation.Application.Features.Portal.School.Stocktake.Queries;
+using Constellation.Core.Models.Stocktake;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+using static System.Net.WebRequestMethods;
 
 [Route("api/[controller]")]
 public class StocktakeController : BaseAPIController
@@ -16,6 +21,29 @@ public class StocktakeController : BaseAPIController
         _logger = logger.ForContext<StocktakeController>();
     }
 
+    [HttpGet("Events")]
+    public async Task<List<StocktakeEventsForList>> GetEvents()
+    {
+        var user = await GetCurrentUser();
+
+        _logger.Information("Requested to retrieve list of current stocktake events by user {user}", user.DisplayName);
+
+        var events = await _mediator.Send(new GetCurrentStocktakeEventsQuery());
+
+        return events.ToList();
+    }
+
+    [HttpGet("Events/{eventId:guid}/Sightings/{schoolCode}")]
+    public async Task<List<StocktakeSightingsForList>> GetSightings([FromRoute] Guid eventId, [FromRoute] string schoolCode)
+    {
+        var user = await GetCurrentUser();
+
+        _logger.Information("Requested to retrieve list of sightings for school {code} for event {event} by user {user}", schoolCode, eventId, user.DisplayName);
+
+        var sightings = await _mediator.Send(new GetStocktakeSightingsForSchoolQuery { SchoolCode = schoolCode, StocktakeEvent = eventId });
+
+        return sightings.ToList();
+    }
 
     [HttpPost("{id:guid}/Remove")]
     public async Task Remove([FromRoute] Guid StocktakeId, [FromBody] CancelStocktakeSightingCommand Command)
@@ -27,5 +55,15 @@ public class StocktakeController : BaseAPIController
         _logger.Information("Requested to remove Stocktake Sighting by {user} with details {@details}", user.DisplayName, Command);
 
         await _mediator.Send(Command);
+    }
+
+    [HttpPost("Submit")]
+    public async Task SubmitSighting([FromBody] RegisterSightedDeviceForStocktakeCommand command)
+    {
+        var user = await GetCurrentUser();
+
+        _logger.Information("Submitting stocktake sighting by user {user} with details {@details}", user.DisplayName, command);
+
+        await _mediator.Send(command);
     }
 }
