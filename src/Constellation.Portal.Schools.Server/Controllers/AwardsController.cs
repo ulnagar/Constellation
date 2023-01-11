7c187dd11b0f217de05c273c6a93f23e70eb8a9e
Ben <ben@hillsley.dev>
2023-01-11 14:08:12 +1100

    Migrated awards section.

---
+++ src/Constellation.Portal.Schools.Server/Controllers/AwardsController.cs
@@ -0,0 +1,32 @@
+﻿namespace Constellation.Portal.Schools.Server.Controllers;
+
+using Constellation.Application.Features.Awards.Models;
+using Constellation.Application.Features.Awards.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+[Route("api/[controller]")]
+public class AwardsController : BaseAPIController
+{
+    private readonly IMediator _mediator;
+    private readonly Serilog.ILogger _logger;
+
+    public AwardsController(IMediator mediator, Serilog.ILogger logger)
+    {
+        _mediator = mediator;
+        _logger = logger.ForContext<AwardsController>();
+    }
+
+    [HttpGet("ForStudent/{studentId}")]
+    public async Task<List<StudentAwardsForList>> GetForSchool(string studentId)
+    {
+        var user = await GetCurrentUser();
+
+        _logger.Information("Requested to get awards for student {studentId} by user {user}", studentId, user.DisplayName);
+
+        var awards = await _mediator.Send(new GetAwardsForStudentQuery { StudentId = studentId });
+
+        return awards.ToList();
+    }
+
+}
--- src/Constellation.Portal.Schools.Server/Controllers/StudentsController.cs
+++ src/Constellation.Portal.Schools.Server/Controllers/StudentsController.cs
@@ -1,6 +1,7 @@
 ﻿namespace Constellation.Portal.Schools.Server.Controllers;
 
 using Constellation.Application.DTOs;
+using Constellation.Application.Features.Portal.School.Home.Models;
 using Constellation.Application.Features.Portal.School.Home.Queries;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
@@ -21,6 +22,18 @@ public class StudentsController : BaseAPIController
     public async Task<List<StudentDto>> GetForSchool(string schoolCode)
     {
         var students = await _mediator.Send(new GetStudentsFromSchoolQuery { SchoolCode = schoolCode });
+
+        return students.ToList();
+    }
+
+    [HttpGet("ForDropdown/{schoolCode}")]
+    public async Task<List<StudentFromSchoolForDropdownSelection>> GetStudentsForDropdown([FromRoute] string schoolCode)
+    {
+        var user = await GetCurrentUser();
+
+        _logger.Information("Requested to retrieve students for school {schoolCode} by user {user}", schoolCode, user.DisplayName);
+
+        var students = await _mediator.Send(new GetStudentsFromSchoolForSelectionQuery { SchoolCode = schoolCode });
 
         return students.ToList();
     }
