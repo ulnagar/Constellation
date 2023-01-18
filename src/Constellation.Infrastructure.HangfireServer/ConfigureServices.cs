2807abf9f93e0efd668611502b4a9f77161664d4
Ben <ben@hillsley.dev>
2023-01-18 12:38:20 +1100

    Refactor the HangfireServer project for the new backend.

--- src/Constellation.Infrastructure.HangfireServer/ConfigureServices.cs
+++ src/Constellation.Infrastructure.HangfireServer/ConfigureServices.cs
@@ -1,13 +1,21 @@
-﻿using Constellation.Application.Models.Identity;
+﻿namespace Constellation.Infrastructure.HangfireServer;
+
+using Constellation.Application.Interfaces.Services;
+using Constellation.Application.Models.Identity;
+using Constellation.Infrastructure.HangfireServer.Services;
 using Constellation.Infrastructure.Persistence.ConstellationContext;
+using Constellation.Infrastructure.Persistence.ConstellationContext.Repositories;
+using Constellation.Infrastructure.Services;
 using Hangfire;
 using Hangfire.SqlServer;
+using Scrutor;
 
-namespace Constellation.Infrastructure.HangfireServer;
 public static class DependencyInjection
 {
     public static IServiceCollection RegisterServices(this IServiceCollection services, IConfiguration config)
 	{
+        services.AddScoped<ICurrentUserService, CurrentUserService>();
+
         services.AddHangfireServerInfrastructureComponents(config);
 
         services.AddIdentity<AppUser, AppRole>()
@@ -32,6 +40,28 @@ public static class DependencyInjection
             });
 
         services.AddControllersWithViews();
+
+        // Search for an register all the Repository classes that are located at
+        // Constellation.Infrastructure.Persistence.ConstellationContext.Repositories
+        services.Scan(selector =>
+            selector.FromAssemblies(
+                Constellation.Application.AssemblyReference.Assembly,
+                Constellation.Infrastructure.AssemblyReference.Assembly)
+            .AddClasses(classes => classes.InNamespaceOf<UnitOfWork>(), false)
+            .UsingRegistrationStrategy(RegistrationStrategy.Skip)
+            .AsMatchingInterface()
+            .WithScopedLifetime());
+
+        // Search for and register all the Services classes that are located at
+        // Constellation.Infrastructure.Services
+        services.Scan(selector =>
+            selector.FromAssemblies(
+                Constellation.Application.AssemblyReference.Assembly,
+                Constellation.Infrastructure.AssemblyReference.Assembly)
+            .AddClasses(classes => classes.InNamespaceOf<AuthService>(), false)
+            .UsingRegistrationStrategy(RegistrationStrategy.Skip)
+            .AsMatchingInterface()
+            .WithScopedLifetime());
 
         return services;
     }
---
+++ src/Constellation.Infrastructure.HangfireServer/Services/CurrentUserService.cs
@@ -0,0 +1,10 @@
+﻿namespace Constellation.Infrastructure.HangfireServer.Services;
+
+using Constellation.Application.Interfaces.Services;
+
+public class CurrentUserService : ICurrentUserService
+{
+    public string? UserName => "HANGFIRE";
+
+    public bool IsAuthenticated => true;
+}
