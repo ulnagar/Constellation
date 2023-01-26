5aff514318a01a47d90329e0320455ae01e6345b
Ben <ben@hillsley.dev>
2023-01-27 09:59:10 +1100

    Create placeholder domain event handlers for MS Team domain events.

---
+++ src/Constellation.Application/Teams/Events/MicrosoftTeamRegisteredDomainEvent_Placeholder.cs
@@ -0,0 +1,21 @@
+﻿namespace Constellation.Core.DomainEvents;
+
+using Constellation.Application.Abstractions.Messaging;
+using System.Threading;
+using System.Threading.Tasks;
+
+internal sealed class MicrosoftTeamRegisteredDomainEvent_Placeholder
+    : IDomainEventHandler<MicrosoftTeamRegisteredDomainEvent>
+{
+    private readonly Serilog.ILogger _logger;
+
+    public MicrosoftTeamRegisteredDomainEvent_Placeholder(Serilog.ILogger logger)
+    {
+        _logger = logger.ForContext<MicrosoftTeamRegisteredDomainEvent>();
+    }
+
+    public async Task Handle(MicrosoftTeamRegisteredDomainEvent notification, CancellationToken cancellationToken)
+    {
+        _logger.Information("Microsoft Team has been registered: {@details}", notification);
+    }
+}
