b66ec39299fe4b9416bffeb9e8829aefba86a70a
Ben <ben@hillsley.au>
2023-03-03 15:54:04 +1100

    Add Unit Tests for new domain models.

---
+++ tests/Constellation.Core.Tests.Unit/Models/Covers/ClassCoverTests.cs
@@ -0,0 +1,82 @@
+﻿using Constellation.Core.DomainEvents;
+using Constellation.Core.Models.Covers;
+
+namespace Constellation.Core.Tests.Unit.Models.Covers;
+
+public class ClassCoverTests
+{
+    [Fact]
+    public void EditDates_ShouldRaiseCorrectDomainEvent_WhenOnlyStartDateChanged()
+    {
+        // Arrange
+        var sut = ClassCover.Create(
+            Guid.NewGuid(),
+            1,
+            DateOnly.FromDateTime(DateTime.Today),
+            DateOnly.FromDateTime(DateTime.Today.AddMonths(1)),
+            ValueObjects.CoverTeacherType.Staff,
+            "1");
+
+        sut.ClearDomainEvents();
+
+        // Act
+        sut.EditDates(
+            DateOnly.FromDateTime(DateTime.Today.AddDays(2)),
+            sut.EndDate);
+        var events = sut.GetDomainEvents();
+
+        // Assert
+        events.Should().HaveCount(1);
+        events.First().Should().BeOfType<CoverStartDateChangedDomainEvent>();
+    }
+
+    [Fact]
+    public void EditDates_ShouldRaiseCorrectDomainEvent_WhenOnlyEndDateChanged()
+    {        
+        // Arrange
+        var sut = ClassCover.Create(
+            Guid.NewGuid(),
+            1,
+            DateOnly.FromDateTime(DateTime.Today),
+            DateOnly.FromDateTime(DateTime.Today.AddMonths(1)),
+            ValueObjects.CoverTeacherType.Staff,
+            "1");
+
+        sut.ClearDomainEvents();
+
+        // Act
+        sut.EditDates(
+            sut.StartDate,
+            DateOnly.FromDateTime(DateTime.Today.AddDays(2)));
+        var events = sut.GetDomainEvents();
+
+        // Assert
+        events.Should().HaveCount(1);
+        events.First().Should().BeOfType<CoverEndDateChangedDomainEvent>();
+    }
+
+    [Fact]
+    public void EditDates_ShouldRaiseCorrectDomainEvent_WhenBothStartAndEndDatesChanged()
+    {
+        // Arrange
+        var sut = ClassCover.Create(
+            Guid.NewGuid(),
+            1,
+            DateOnly.FromDateTime(DateTime.Today),
+            DateOnly.FromDateTime(DateTime.Today.AddMonths(1)),
+            ValueObjects.CoverTeacherType.Staff,
+            "1");
+
+        sut.ClearDomainEvents();
+
+        // Act
+        sut.EditDates(
+            DateOnly.FromDateTime(DateTime.Today.AddDays(2)),
+            DateOnly.FromDateTime(DateTime.Today.AddDays(2)));
+        var events = sut.GetDomainEvents();
+
+        // Assert
+        events.Should().HaveCount(1);
+        events.First().Should().BeOfType<CoverStartAndEndDatesChangedDomainEvent>();
+    }
+}
---
+++ tests/Constellation.Core.Tests.Unit/Models/MandatoryTraining/TrainingCompletionTests.cs
@@ -0,0 +1,93 @@
+﻿using Constellation.Core.Enums;
+using Constellation.Core.Models.MandatoryTraining;
+
+namespace Constellation.Core.Tests.Unit.Models.MandatoryTraining;
+
+public class TrainingCompletionTests
+{
+    [Fact]
+    public void MarkNotRequired_DoesNothing_WhenModuleDoesNotAllowNotRequiredEntries()
+    {
+        // Arrange
+        var module = new TrainingModule(Guid.NewGuid())
+        {
+            Name = "Test Module",
+            Expiry = TrainingModuleExpiryFrequency.OnceOff,
+            Url = string.Empty,
+            CanMarkNotRequired = false
+        };
+
+        var sut = new TrainingCompletion(Guid.NewGuid())
+        {
+            StaffId = "1",
+            TrainingModuleId = module.Id,
+            Module = module
+        };
+
+        sut.SetCompletedDate(DateTime.Today);
+
+        // Act
+        sut.MarkNotRequired();
+
+        // Assert
+        sut.NotRequired.Should().Be(false);
+        sut.CompletedDate.Should().NotBe(null);
+    }
+
+    [Fact]
+    public void MarkNotRequired_ShouldUpdateEntity_WhenModuleAllowsNotRequiredEntries()
+    {
+        // Arrange
+        var module = new TrainingModule(Guid.NewGuid())
+        {
+            Name = "Test Module",
+            Expiry = TrainingModuleExpiryFrequency.OnceOff,
+            Url = string.Empty,
+            CanMarkNotRequired = true
+        };
+
+        var sut = new TrainingCompletion(Guid.NewGuid())
+        {
+            StaffId = "1",
+            TrainingModuleId = module.Id,
+            Module = module
+        };
+
+        sut.SetCompletedDate(DateTime.Today);
+
+        // Act
+        sut.MarkNotRequired();
+
+        // Assert
+        sut.NotRequired.Should().Be(true);
+        sut.CompletedDate.Should().Be(null);
+    }
+
+    [Fact]
+    public void SetCompletedDate_ShouldUpdateNotRequiredField_WhenSettingANewCompletionDate()
+    {
+        // Arrange
+        var module = new TrainingModule(Guid.NewGuid())
+        {
+            Name = "Test Module",
+            Expiry = TrainingModuleExpiryFrequency.OnceOff,
+            Url = string.Empty,
+            CanMarkNotRequired = true
+        };
+
+        var sut = new TrainingCompletion(Guid.NewGuid())
+        {
+            StaffId = "1",
+            TrainingModuleId = module.Id,
+            Module = module
+        };
+
+        sut.MarkNotRequired();
+
+        // Act
+        sut.SetCompletedDate(DateTime.Today);
+
+        // Assert
+        sut.NotRequired.Should().Be(false);
+    }
+}
