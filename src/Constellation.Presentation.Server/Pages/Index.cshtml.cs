fa975aa3f76a6d2f827f15220cc2a758e78f2d46
Ben <ben@hillsley.dev>
2023-04-03 16:20:44 +1000

    Customise the landing page with new design

--- src/Constellation.Presentation.Server/Pages/Index.cshtml.cs
+++ src/Constellation.Presentation.Server/Pages/Index.cshtml.cs
@@ -1,12 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
-using System;
+﻿namespace Constellation.Presentation.Server.Pages;
 
-namespace Constellation.Presentation.Server.Pages
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+public class IndexModel : PageModel
 {
-    public class IndexModel : PageModel
+    public void OnGet()
     {
-        public void OnGet()
-        {
-        }
     }
 }
