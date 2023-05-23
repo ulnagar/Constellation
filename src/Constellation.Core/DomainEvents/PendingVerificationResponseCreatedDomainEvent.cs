56bc5b8baadd3e284137510c947a1c9c80783f4b
Ben <ben@hillsley.au>
2023-05-23 11:25:15 +1000

    Finalise conversion of absences models to aggregates.

---
+++ src/Constellation.Core/DomainEvents/PendingVerificationResponseCreatedDomainEvent.cs
@@ -0,0 +1,9 @@
+﻿namespace Constellation.Core.DomainEvents;
+
+using Constellation.Core.Models.Identifiers;
+
+public sealed record PendingVerificationResponseCreatedDomainEvent(
+    DomainEventId Id,
+    AbsenceResponseId ResponseId,
+    AbsenceId AbsenceId)
+    : DomainEvent(Id);
