a2cbbe8844782200a05a4cf824c8d7cd92056a30
Ben <ben@hillsley.au>
2023-05-23 11:25:15 +1000

    Finalise conversion of absences models to aggregates.

--- src/Constellation.Core/Models/Absences/Response.cs
+++ src/Constellation.Core/Models/Absences/Response.cs
@@ -5,7 +5,26 @@ using System;
 
 public class Response
 {
+    private Response(
+        AbsenceResponseId id,
+        AbsenceId absenceId,
+        DateTime receivedAt,
+        ResponseType type,
+        string from,
+        string explanation)
+    {
+        Id = id;
+        AbsenceId = absenceId;
+        ReceivedAt = receivedAt;
+        Type = type;
+        From = from;
+        Explanation = explanation;
 
+        if (Type == ResponseType.Student)
+            VerificationStatus = ResponseVerificationStatus.Pending;
+        else
+            VerificationStatus = ResponseVerificationStatus.NotRequired;
+    }
 
     public AbsenceResponseId Id { get; private set; }
     public AbsenceId AbsenceId { get; private set; }
@@ -18,4 +37,47 @@ public class Response
     public DateTime? VerifiedAt { get; private set; }
     public string VerificationComment { get; private set; }
     public bool Forwarded { get; private set; }
+
+    public static Response Create(
+        AbsenceId absenceId,
+        DateTime receivedAt,
+        ResponseType type,
+        string from,
+        string explanation)
+    {
+        var response = new Response(
+            new AbsenceResponseId(),
+            absenceId,
+            receivedAt,
+            type,
+            from,
+            explanation);
+
+        return response;
+    }
+
+    public void VerifyResponse(
+        string verifier,
+        string comment)
+    {
+        VerificationStatus = ResponseVerificationStatus.Verified;
+        VerificationComment = comment;
+        Verifier = verifier;
+        VerifiedAt = DateTime.Now;
+    }
+
+    public void RejectResponse(
+        string verifier,
+        string comment)
+    {
+        VerificationStatus = ResponseVerificationStatus.Rejected;
+        VerificationComment = comment;
+        Verifier = verifier;
+        VerifiedAt = DateTime.Now;
+    }
+
+    public void MarkForwarded()
+    {
+        Forwarded = true;
+    }
 }
