e98ff3d0f44c8e9a8f4650e09c437e0eb7f65e58
Ben <ben@hillsley.dev>
2023-07-07 09:54:47 +1000

    Finish updating the Teachers Absence Portal pages in the Presentation project.

---
+++ src/Constellation.Application/MissedWork/CreateClassworkNotificationResponse/CreateClassworkNotificationResponseCommand.cs
@@ -0,0 +1,10 @@
+﻿namespace Constellation.Application.MissedWork.CreateClassworkNotificationResponse;
+
+using Constellation.Application.Abstractions.Messaging;
+using Constellation.Core.Models.Identifiers;
+
+public sealed record CreateClassworkNotificationResponseCommand(
+    ClassworkNotificationId NotificationId,
+    string TeacherEmailAddress,
+    string Description)
+    : ICommand;
