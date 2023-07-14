b008f4d9f83d60b590e3831706b46a0274b5a39a
Ben <ben@hillsley.au>
2023-07-14 15:23:22 +1000

    Continue editing the generated database migration to reduce data loss.

--- src/Constellation.Infrastructure/Persistence/ConstellationContext/Migrations/20230707041145_ConvertAbsencesToAggregate.cs
+++ src/Constellation.Infrastructure/Persistence/ConstellationContext/Migrations/20230707041145_ConvertAbsencesToAggregate.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Microsoft.EntityFrameworkCore.Migrations;
+using OfficeOpenXml.Style;
 
 #nullable disable
 
@@ -11,9 +13,208 @@ namespace Constellation.Infrastructure.Persistence.ConstellationContext.Migratio
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            // Absences Table
+             
             migrationBuilder.DropForeignKey(
                 name: "FK_AbsenceClassworkNotification_Absences_AbsencesId",
                 table: "AbsenceClassworkNotification");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Absences_Offerings_OfferingId",
+                table: "Absences");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Absences_Students_StudentId",
+                table: "Absences");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Absences", 
+                table: "Absences");
+
+            migrationBuilder.RenameTable(
+                name: "Absences",
+                newName: "Absences_Absences");
+
+            migrationBuilder.RenameColumn(
+                name: "DateScanned",
+                table: "Absences_Absences",
+                newName: "FirstSeen");
+
+            migrationBuilder.DropColumn(
+                name: "ExternalExplanation",
+                table: "Absences_Absences");
+
+            migrationBuilder.DropColumn(
+                name: "ExternalExplanationSource",
+                table: "Absences_Absences");
+
+            migrationBuilder.DropColumn(
+                name: "ExternallyExplained",
+                table: "Absences_Absences");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Absences_Absences",
+                table: "Absences_Absences",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Absences_Absences_Offerings_OfferingId",
+                table: "Absences_Absences",
+                column: "OfferingId",
+                principalTable: "Offerings",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Absences_Absences_Students_StudentId",
+                table: "Absences_Absences",
+                column: "StudentId",
+                principalTable: "Students",
+                principalColumn: "StudentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Absences_Absences_OfferingId",
+                table: "Absences_Absences",
+                column: "OfferingId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Absences_Absences_StudentId",
+                table: "Absences_Absences",
+                column: "StudentId");
+
+            // AbsenceNotifications table
+
+            migrationBuilder.DropIndex(
+                name: "IX_AbsenceNotification_AbsenceId",
+                table: "AbsenceNotification");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AbsenceNotification_Absences_AbsenceId",
+                table: "AbsenceNotification");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_AbsenceNotification",
+                table: "AbsenceNotification");
+
+            migrationBuilder.RenameTable(
+                name: "AbsenceNotification",
+                newName: "Absences_Notifications");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Absences_Notifications",
+                table: "Absences_Notifications",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Absences_Notifications_Absences_Absences_AbsenceId",
+                table: "Absences_Notifications",
+                column: "AbsenceId",
+                principalTable: "Absences_Absences",
+                principalColumn: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Absences_Notifications_AbsenceId",
+                table: "Absences_Notifications",
+                column: "AbsenceId");
+
+            // AbsenceResponse table
+
+            migrationBuilder.DropIndex(
+                name: "IX_AbsenceResponse_AbsenceId",
+                table: "AbsenceResponse");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AbsenceResponse_Absences_AbsenceId",
+                table: "AbsenceResponse");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_AbsenceResponse",
+                table: "AbsenceResponse");
+
+            migrationBuilder.RenameTable(
+                name: "AbsenceResponse",
+                newName: "Absences_Responses");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Absences_Responses",
+                table: "Absences_Responses",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Absences_Responses_Absences_Absences_AbsenceId",
+                table: "Absences_Responses",
+                column: "AbsenceId",
+                principalTable: "Absences_Absences",
+                principalColumn: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Absences_Responses_AbsenceId",
+                table: "Absences_Responses",
+                column: "AbsenceId");
+
+            // ClassworkNotifications table
+
+            migrationBuilder.DropIndex(
+                name: "IX_ClassworkNotifications_OfferingId",
+                table: "ClassworkNotifications");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ClassworkNotifications_StaffId",
+                table: "ClassworkNotifications");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ClassworkNotifications_Offerings_OfferingId",
+                table: "ClassworkNotifications");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ClassworkNotifications_Staff_StaffId",
+                table: "ClassworkNotifications");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ClassworkNotifications",
+                table: "ClassworkNotifications");
+
+            migrationBuilder.RenameTable(
+                name: "ClassworkNotifications",
+                newName: "MissedWork_ClassworkNotifications");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CompletedBy",
+                table: "MissedWork_ClassworkNotifications",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_MissedWork_ClassworkNotifications",
+                table: "MissedWork_ClassworkNotifications",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MissedWork_ClassworkNotifications_Offerings_OfferingId",
+                table: "MissedWork_ClassworkNotifications",
+                column: "OfferingId",
+                principalTable: "Offerings",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MissedWork_ClassworkNotifications_Staff_StaffId",
+                table: "MissedWork_ClassworkNotifications",
+                column: "StaffId",
+                principalTable: "Staff",
+                principalColumn: "StaffId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MissedWork_ClassworkNotifications_OfferingId",
+                table: "MissedWork_ClassworkNotifications",
+                column: "OfferingId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MissedWork_ClassworkNotifications_StaffId",
+                table: "MissedWork_ClassworkNotifications",
+                column: "StaffId");
+
+            // AbsenceClassworkNotification table
 
             migrationBuilder.DropForeignKey(
                 name: "FK_AbsenceClassworkNotification_ClassworkNotifications_ClassworkNotificationsId",
@@ -22,18 +223,6 @@ namespace Constellation.Infrastructure.Persistence.ConstellationContext.Migratio
             migrationBuilder.DropForeignKey(
                 name: "FK_ClassworkNotificationStaff_ClassworkNotifications_ClassworkNotificationsId",
                 table: "ClassworkNotificationStaff");
-
-            migrationBuilder.DropTable(
-                name: "AbsenceNotification");
-
-            migrationBuilder.DropTable(
-                name: "AbsenceResponse");
-
-            migrationBuilder.DropTable(
-                name: "ClassworkNotifications");
-
-            migrationBuilder.DropTable(
-                name: "Absences");
 
             migrationBuilder.RenameColumn(
                 name: "ClassworkNotificationsId",
@@ -50,176 +239,7 @@ namespace Constellation.Infrastructure.Persistence.ConstellationContext.Migratio
                 table: "AbsenceClassworkNotification",
                 newName: "IX_AbsenceClassworkNotification_ClassworkNotificationId");
 
-            // Rename table to "Absences_Absences" from "Absences"
-            // Rename column to "FirstSeen" from "DateScanned"
-            // Drop column "ExternalExplanation"
-            // Drop column "ExternalExplanationSource"
-            // Drop column "ExternallyExplained"
-            // Rename primary key to "PK_Absences_Absences" from "PK_Absences"
-            // Rename foreign key to "FK_Absences_Absences_Offerings_OfferingId" from "FK_Absences_Offerings_OfferingId"
-            // Rename foreign key to "FK_Absences_Absences_Students_StudentId" from "FK_Absences_Students_StudentId"
 
-            migrationBuilder.CreateTable(
-                name: "Absences_Absences",
-                columns: table => new
-                {
-                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
-                    Type = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    StudentId = table.Column<string>(type: "nvarchar(450)", nullable: true),
-                    OfferingId = table.Column<int>(type: "int", nullable: false),
-                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
-                    PeriodName = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    PeriodTimeframe = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    AbsenceLength = table.Column<int>(type: "int", nullable: false),
-                    AbsenceTimeframe = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    AbsenceReason = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    StartTime = table.Column<TimeSpan>(type: "time", nullable: false),
-                    EndTime = table.Column<TimeSpan>(type: "time", nullable: false),
-                    FirstSeen = table.Column<DateTime>(type: "datetime2", nullable: false),
-                    LastSeen = table.Column<DateTime>(type: "datetime2", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Absences_Absences", x => x.Id);
-                    table.ForeignKey(
-                        name: "FK_Absences_Absences_Offerings_OfferingId",
-                        column: x => x.OfferingId,
-                        principalTable: "Offerings",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                    table.ForeignKey(
-                        name: "FK_Absences_Absences_Students_StudentId",
-                        column: x => x.StudentId,
-                        principalTable: "Students",
-                        principalColumn: "StudentId");
-                });
-
-            // Rename table to "MissedWork_ClassworkNotifications" from "ClassworkNotifications"
-            // Add column "CompletedBy" as string, nvarchar(max), true
-            // Rename primary key to "PK_MissedWork_ClassworkNotifications" from "PK_ClassworkNotifications"
-            // Rename foreign key to "FK_MissedWork_ClassworkNotifications_Offerings_OfferingId" from "FK_ClassworkNotifications_Offerings_OfferingId"
-            // Rename foreign key to "FK_MissedWoek_ClassworkNotifications_Staff_StaffId" from "FK_ClassworkNotifications_Staff_StaffId"
-
-            migrationBuilder.CreateTable(
-                name: "MissedWork_ClassworkNotifications",
-                columns: table => new
-                {
-                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
-                    GeneratedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
-                    OfferingId = table.Column<int>(type: "int", nullable: false),
-                    AbsenceDate = table.Column<DateTime>(type: "datetime2", nullable: false),
-                    IsCovered = table.Column<bool>(type: "bit", nullable: false),
-                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    CompletedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    StaffId = table.Column<string>(type: "nvarchar(450)", nullable: true),
-                    CompletedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_MissedWork_ClassworkNotifications", x => x.Id);
-                    table.ForeignKey(
-                        name: "FK_MissedWork_ClassworkNotifications_Offerings_OfferingId",
-                        column: x => x.OfferingId,
-                        principalTable: "Offerings",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                    table.ForeignKey(
-                        name: "FK_MissedWork_ClassworkNotifications_Staff_StaffId",
-                        column: x => x.StaffId,
-                        principalTable: "Staff",
-                        principalColumn: "StaffId");
-                });
-
-            // Rename table to "Absences_Notifications" from "AbsenceNotification"
-            // Modify column "AbsenceId" to set nullable: true
-            // Rename primary key to "PK_Absences_Notifications" from "PK_AbsenceNotification"
-            // Rename foreign key to "FK_Absences_Notifications_Absences_Absences_AbsenceId" from "FK_AbsenceNotification_Absences_AbsenceId"
-
-            migrationBuilder.CreateTable(
-                name: "Absences_Notifications",
-                columns: table => new
-                {
-                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
-                    AbsenceId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
-                    Type = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    OutgoingId = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    SentAt = table.Column<DateTime>(type: "datetime2", nullable: false),
-                    Message = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    Recipients = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    ConfirmedDelivered = table.Column<bool>(type: "bit", nullable: false),
-                    DeliveredAt = table.Column<DateTime>(type: "datetime2", nullable: true),
-                    DeliveredMessageIds = table.Column<string>(type: "nvarchar(max)", nullable: true)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Absences_Notifications", x => x.Id);
-                    table.ForeignKey(
-                        name: "FK_Absences_Notifications_Absences_Absences_AbsenceId",
-                        column: x => x.AbsenceId,
-                        principalTable: "Absences_Absences",
-                        principalColumn: "Id");
-                });
-
-            // Rename table to "Absences_Responses" from "AbsenceResponse"
-            // Modify column "AbsenceId" to set nullable: true
-            // Rename primary key to "PK_Absences_Responses" from "PK_AbsenceResponse"
-            // Rename foreign key to "FK_Absences_Responses_Absences_Absences_AbsenceId"
-
-            migrationBuilder.CreateTable(
-                name: "Absences_Responses",
-                columns: table => new
-                {
-                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
-                    AbsenceId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
-                    ReceivedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
-                    Type = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    From = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    Explanation = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    VerificationStatus = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    Verifier = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    VerifiedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
-                    VerificationComment = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    Forwarded = table.Column<bool>(type: "bit", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Absences_Responses", x => x.Id);
-                    table.ForeignKey(
-                        name: "FK_Absences_Responses_Absences_Absences_AbsenceId",
-                        column: x => x.AbsenceId,
-                        principalTable: "Absences_Absences",
-                        principalColumn: "Id");
-                });
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Absences_Absences_OfferingId",
-                table: "Absences_Absences",
-                column: "OfferingId");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Absences_Absences_StudentId",
-                table: "Absences_Absences",
-                column: "StudentId");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Absences_Notifications_AbsenceId",
-                table: "Absences_Notifications",
-                column: "AbsenceId");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_Absences_Responses_AbsenceId",
-                table: "Absences_Responses",
-                column: "AbsenceId");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_MissedWork_ClassworkNotifications_OfferingId",
-                table: "MissedWork_ClassworkNotifications",
-                column: "OfferingId");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_MissedWork_ClassworkNotifications_StaffId",
-                table: "MissedWork_ClassworkNotifications",
-                column: "StaffId");
 
             migrationBuilder.AddForeignKey(
                 name: "FK_AbsenceClassworkNotification_Absences_Absences_AbsencesId",
