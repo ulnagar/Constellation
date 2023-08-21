6141c1e015f49f3a8b177faf3f86efa75954e4d7
Ben <ben@hillsley.dev>
2023-08-21 16:23:18 +1000

    Refactor OfferingResource to link systems to the Offering

---
+++ src/Constellation.Core/Models/Subjects/Resource.cs
@@ -0,0 +1,73 @@
+﻿namespace Constellation.Core.Models.Subjects;
+
+using Constellation.Core.Models.Subjects.Identifiers;
+using Constellation.Core.Models.Subjects.ValueObjects;
+using System;
+
+public abstract class Resource
+{
+    public ResourceId Id { get; protected set; }
+    public ResourceType Type { get; protected set; }
+    public int OfferingId { get; protected set; }
+    public virtual CourseOffering Offering { get; protected set; }
+    public string ResourceId { get; protected set; }
+    public string Name { get; protected set; }
+    public string Url { get; protected set; }
+}
+
+public sealed class AdobeConnectRoomResource : Resource
+{
+    public AdobeConnectRoomResource(
+        int offeringId,
+        string scoId,
+        string name,
+        string url)
+    {
+        Id = new();
+        Type = ResourceType.AdobeConnectRoom;
+        OfferingId = offeringId;
+        ResourceId = scoId;
+        Name = name;
+        Url = url;
+    }
+
+    public string ScoId => ResourceId;
+}
+
+public sealed class MicrosoftTeamResource : Resource
+{
+    public MicrosoftTeamResource(
+        int offeringId,
+        Guid teamGroupId,
+        string name,
+        string url)
+    {
+        Id = new();
+        Type = ResourceType.MicrosoftTeam;
+        OfferingId = offeringId;
+        ResourceId = teamGroupId.ToString();
+        Name = name;
+        Url = url;
+    }
+
+    public Guid GroupId => new Guid(ResourceId);
+}
+
+public sealed class CanvasCourseResource : Resource
+{
+    public CanvasCourseResource(
+        int offeringId,
+        string canvasCourseId,
+        string name,
+        string url)
+    {
+        Id = new();
+        Type = ResourceType.CanvasCourse;
+        OfferingId = offeringId;
+        ResourceId = canvasCourseId;
+        Name = name;
+        Url = url;
+    }
+
+    public string CourseId => ResourceId;
+}
