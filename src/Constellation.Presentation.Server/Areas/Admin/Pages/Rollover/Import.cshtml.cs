7a2e608c17b16ff548ae9ed52315a52a5b6c8ecf
Ben <ben@hillsley.au>
2023-10-27 15:55:04 +1100

    Move Rollover pages to Admin area.

---
+++ src/Constellation.Presentation.Server/Areas/Admin/Pages/Rollover/Import.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Constellation.Presentation.Server.Areas.Admin.Pages.Rollover
+{
+    public class ImportModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
