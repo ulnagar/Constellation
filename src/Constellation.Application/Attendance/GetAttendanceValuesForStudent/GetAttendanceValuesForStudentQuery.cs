e84ce90534dad7e77f4269967ce5d14fc221efb6
Ben <ben@hillsley.au>
2023-11-11 22:52:26 +1100

    Move pages to new Compliance section under SchoolAdmin

---
+++ src/Constellation.Application/Attendance/GetAttendanceValuesForStudent/GetAttendanceValuesForStudentQuery.cs
@@ -0,0 +1,9 @@
+﻿namespace Constellation.Application.Attendance.GetAttendanceValuesForStudent;
+
+using Abstractions.Messaging;
+using Core.Models.Attendance;
+using System.Collections.Generic;
+
+public sealed record GetAttendanceValuesForStudentQuery(
+    string StudentId)
+    : IQuery<List<AttendanceValue>>;
