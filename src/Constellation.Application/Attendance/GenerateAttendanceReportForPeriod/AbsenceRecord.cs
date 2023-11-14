b8c192a3cdd374c918f483f6d7233c8bad054ef7
Ben <ben@hillsley.dev>
2023-11-14 16:25:02 +1100

    Expand the absence table to include students with low attendance without absences that week.

--- src/Constellation.Application/Attendance/GenerateAttendanceReportForPeriod/AbsenceRecord.cs
+++ src/Constellation.Application/Attendance/GenerateAttendanceReportForPeriod/AbsenceRecord.cs
@@ -1,12 +1,9 @@
 ﻿namespace Constellation.Application.Attendance.GenerateAttendanceReportForPeriod;
 
-using Core.Enums;
 using System;
 
 public sealed record AbsenceRecord(
     string StudentId,
-    string StudentName,
-    Grade Grade,
     string AbsenceReason,
     DateOnly AbsenceDate,
     string AbsenceLesson,
