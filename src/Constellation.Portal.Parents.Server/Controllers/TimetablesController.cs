664d07bb3523f6e6d200e26b8a509926b09b551f
Ben <ben@hillsley.au>
2024-01-08 17:34:41 +1100

    Test and fix Timetables section of Parent Portal

--- src/Constellation.Portal.Parents.Server/Controllers/TimetablesController.cs
+++ src/Constellation.Portal.Parents.Server/Controllers/TimetablesController.cs
@@ -3,6 +3,7 @@
 using Application.Timetables.GetStudentTimetableData;
 using Constellation.Application.DTOs;
 using Constellation.Application.Features.Portal.School.Timetables.Queries;
+using Core.Errors;
 using Core.Shared;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
@@ -10,55 +11,50 @@ using Microsoft.AspNetCore.Mvc;
 [Route("[controller]")]
 public class TimetablesController : BaseAPIController
 {
-	private readonly ILogger<TimetablesController> _logger;
+	private readonly Serilog.ILogger _logger;
 	private readonly IMediator _mediator;
 
-	public TimetablesController(ILogger<TimetablesController> logger, IMediator mediator)
+	public TimetablesController(
+        Serilog.ILogger logger, 
+        IMediator mediator)
 	{
-		_logger = logger;
+		_logger = logger.ForContext<TimetablesController>();
 		_mediator = mediator;
 	}
 
 	[HttpGet("{studentId}")]
-	public async Task<StudentTimetableDataDto> GetStudentTimetable([FromRoute] string studentId)
+	public async Task<ApiResult<StudentTimetableDataDto>> GetStudentTimetable([FromRoute] string studentId)
 	{
-		var authorised = await HasAuthorizedAccessToStudent(_mediator, studentId);
+		bool authorised = await HasAuthorizedAccessToStudent(_mediator, studentId);
 
 		if (!authorised)
-		{
-			return new StudentTimetableDataDto();
-		}
+            return ApiResult.FromResult(Result.Failure<StudentTimetableDataDto>(DomainErrors.Auth.NotAuthorised));
 
 		Result<StudentTimetableDataDto> request = await _mediator.Send(new GetStudentTimetableDataQuery(studentId));
 
-        if (request.IsFailure)
-        {
-            return new StudentTimetableDataDto();
-        }
-
-        return request.Value;
+        return ApiResult.FromResult(request);
     }
 
     [HttpPost("Download/{studentId}")]
     public async Task<IActionResult> GetAttendanceReport([FromRoute] string studentId)
     {
-        var authorised = await HasAuthorizedAccessToStudent(_mediator, studentId);
+        bool authorised = await HasAuthorizedAccessToStudent(_mediator, studentId);
 
         if (!authorised)
         {
-            return BadRequest();
+            return Ok(ApiResult.FromResult(Result.Failure(DomainErrors.Auth.NotAuthorised)));
         }
 
         // Get Timetable Data first
-        var data = await _mediator.Send(new GetStudentTimetableDataQuery(studentId));
+        Result<StudentTimetableDataDto>? data = await _mediator.Send(new GetStudentTimetableDataQuery(studentId));
 
         if (data.IsFailure)
         {
-            return BadRequest();
+            return Ok(ApiResult.FromResult(data));
         }
 
         // We only have one student, so just download that file.
-        var file = await _mediator.Send(new GetStudentTimetableExportQuery { Data = data.Value });
+        FileDto? file = await _mediator.Send(new GetStudentTimetableExportQuery { Data = data.Value });
 
         return File(file.FileData, file.FileType, file.FileName);
     }
