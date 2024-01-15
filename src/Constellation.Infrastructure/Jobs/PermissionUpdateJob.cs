1b609481d95bcef3a7b35c56d710dcab7300fffb
Ben <ben@hillsley.dev>
2024-01-15 15:33:48 +1100

    Update PermissionUpdateJob to remove AdobeConnect links.

--- src/Constellation.Infrastructure/Jobs/PermissionUpdateJob.cs
+++ src/Constellation.Infrastructure/Jobs/PermissionUpdateJob.cs
@@ -1,74 +1,40 @@
 ﻿namespace Constellation.Infrastructure.Jobs;
 
+using Application.DTOs;
 using Constellation.Application.Features.API.Operations.Commands;
 using Constellation.Application.Interfaces.Jobs;
 using Constellation.Application.Interfaces.Repositories;
-using Constellation.Application.Interfaces.Services;
 using Constellation.Core.Models;
 using MediatR;
 using System;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
 internal sealed class PermissionUpdateJob : IPermissionUpdateJob
 {
     private readonly IUnitOfWork _unitOfWork;
-    private readonly IAdobeConnectService _adobeConnectService;
-    private readonly IOperationService _operationsService;
     private readonly IMediator _mediator;
     private readonly ILogger _logger;
 
-    private Guid JobId { get; set; }
-
     public PermissionUpdateJob(
-        IUnitOfWork unitOfWork, 
-        IAdobeConnectService adobeConnectService,
-        IOperationService operationsService, 
+        IUnitOfWork unitOfWork,
         IMediator mediator,
         ILogger logger)
     {
         _unitOfWork = unitOfWork;
-        _adobeConnectService = adobeConnectService;
-        _operationsService = operationsService;
         _mediator = mediator;
         _logger = logger.ForContext<IPermissionUpdateJob>();
     }
 
     public async Task StartJob(Guid jobId, CancellationToken token)
     {
-        JobId = jobId;
+        _logger.ForContext("JobId", jobId);
 
-        _logger.Information("{id}: Searching for users without Adobe Connect Principal Id information...", jobId);
-        var updatedUsers = await _adobeConnectService.UpdateUsers();
-        _logger.Information("{id}: Found Adobe Connect Principal Id information for {count} users.", jobId, updatedUsers.Count());
-
-        _logger.Information("{id}: Searching for operations...", jobId);
-        var operations = await _unitOfWork.AdobeConnectOperations.AllToProcess();
-        var canvasOperations = await _unitOfWork.CanvasOperations.AllToProcess();
-        _logger.Information("{id}: Found {count} operations to process.", jobId, operations.Count);
-
-        foreach (var operation in operations)
-        {
-            if (token.IsCancellationRequested)
-                return;
-
-            await ProcessOperation(operation);
-        }
-
-        _logger.Information("{id}: Searching for overdue operations...", jobId);
-        operations = await _unitOfWork.AdobeConnectOperations.AllOverdue();
-        _logger.Information("{id}: Found {count} overdue operations to process.", jobId, operations.Count);
-
-        foreach (var operation in operations)
-        {
-            if (token.IsCancellationRequested)
-                return;
-
-            await ProcessOperation(operation);
-        }
-
-        foreach (var operation in canvasOperations)
+        _logger.Information("Searching for operations...");
+        ICollection<CanvasOperation> operations = await _unitOfWork.CanvasOperations.AllToProcess();
+        _logger.Information("Found {count} operations to process.", jobId);
+        
+        foreach (CanvasOperation operation in operations)
         {
             if (token.IsCancellationRequested)
                 return;
@@ -81,23 +47,13 @@ internal sealed class PermissionUpdateJob : IPermissionUpdateJob
         if (token.IsCancellationRequested)
             return;
 
-        _logger.Information("{id}: Completed", jobId);
+        _logger.Information("Completed");
     }
-
-
-    private async Task ProcessOperation(AdobeConnectOperation operation)
-    {
-        var result = await _adobeConnectService.ProcessOperation(operation);
-        if (result.Success)
-            await _operationsService.MarkAdobeConnectOperationComplete(operation.Id);
-        foreach (var line in result.Errors)
-            _logger.Information("{id}: {operation}: {line}", JobId, operation, line);
-    }
-
+    
     private async Task ProcessOperation(CanvasOperation operation)
     {
-        var result = await _mediator.Send(new ProcessCanvasOperationCommand { Operation = operation });
-        foreach (var line in result.Errors)
-            _logger.Information("{id}: {operation}: {line}", JobId, operation, line);
+        ServiceOperationResult<CanvasOperation> result = await _mediator.Send(new ProcessCanvasOperationCommand { Operation = operation });
+        foreach (string line in result.Errors)
+            _logger.Information("{operation}: {line}", operation, line);
     }
 }
