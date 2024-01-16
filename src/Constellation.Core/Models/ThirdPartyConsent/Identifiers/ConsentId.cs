5cb27e5bc8d674bfe39d43a31805ab4cd6bf362a
Ben <ben@hillsley.dev>
2024-01-16 16:02:05 +1100

    Start building models for the ThirdPartyConsent feature

---
+++ src/Constellation.Core/Models/ThirdPartyConsent/Identifiers/ConsentId.cs
@@ -0,0 +1,15 @@
+﻿namespace Constellation.Core.Models.ThirdPartyConsent.Identifiers;
+
+using System;
+
+public sealed record ConsentId(Guid Value)
+{
+    public static ConsentId FromValue(Guid value) =>
+        new(value);
+
+    public ConsentId()
+        : this(Guid.NewGuid()) { }
+
+    public override string ToString() =>
+        Value.ToString();
+}
