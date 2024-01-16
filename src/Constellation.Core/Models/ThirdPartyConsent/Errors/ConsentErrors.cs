b37973ee4a9fd06bdafb2112608b8a2d78a6f45b
Ben <ben@hillsley.dev>
2024-01-16 16:02:05 +1100

    Start building models for the ThirdPartyConsent feature

---
+++ src/Constellation.Core/Models/ThirdPartyConsent/Errors/ConsentErrors.cs
@@ -0,0 +1,20 @@
+﻿namespace Constellation.Core.Models.ThirdPartyConsent.Errors;
+
+using Shared;
+
+public static class ConsentErrors
+{
+    public static class ConsentMethod
+    {
+        public static Error ValueEmpty = new(
+            "Consent.ConsentMethod.ValueEmpty",
+            "A valid value must be provided for the Consent Method");
+    }
+
+    public static class Transaction
+    {
+        public static Error NoResponses = new(
+            "Consent.Transaction.NoResponses",
+            "At least one response must be included when submitting a Consent Transaction");
+    }
+}
---
+++ src/Constellation.Core/Models/ThirdPartyConsent/Events/ConsentTransactionReceivedDomainEvent.cs
@@ -0,0 +1,12 @@
+﻿namespace Constellation.Core.Models.ThirdPartyConsent.Events;
+
+using Constellation.Core.DomainEvents;
+using Constellation.Core.Models.Identifiers;
+using Identifiers;
+
+public sealed record ConsentTransactionReceivedDomainEvent(
+    DomainEventId Id,
+    ConsentTransactionId TransactionId)
+    : DomainEvent(Id);
+
+// -> When a transaction is created, generate a PDF document for this and email to the parent who submitted the transaction, for their records.
