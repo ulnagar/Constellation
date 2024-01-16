2ac18a48125b0c30d8f762cdd834e3963643fa54
Ben <ben@hillsley.dev>
2024-01-16 16:02:05 +1100

    Start building models for the ThirdPartyConsent feature

---
+++ src/Constellation.Core/Models/ThirdPartyConsent/Identifiers/ApplicationId.cs
@@ -0,0 +1,15 @@
+﻿namespace Constellation.Core.Models.ThirdPartyConsent.Identifiers;
+
+using System;
+
+public sealed record ApplicationId(Guid Value)
+{
+    public static ApplicationId FromValue(Guid value) =>
+        new(value);
+
+    public ApplicationId()
+        : this(Guid.NewGuid()) { }
+
+    public override string ToString() =>
+        Value.ToString();
+}
---
+++ src/Constellation.Core/Models/ThirdPartyConsent/Transaction.cs
@@ -0,0 +1,79 @@
+﻿namespace Constellation.Core.Models.ThirdPartyConsent;
+
+using Enums;
+using Errors;
+using Events;
+using Identifiers;
+using Primitives;
+using Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using ApplicationId = Identifiers.ApplicationId;
+
+public sealed class Transaction : AggregateRoot
+{
+    private readonly List<Consent> _consents = new();
+
+    private Transaction(
+        string studentId,
+        string submittedBy,
+        DateTime submittedAt,
+        ConsentMethod submissionMethod,
+        string submissionNotes)
+    {
+        Id = new();
+        StudentId = studentId;
+        SubmittedBy = submittedBy;
+        SubmittedAt = submittedAt;
+        SubmissionMethod = submissionMethod;
+        SubmissionNotes = submissionNotes;
+    }
+
+    public ConsentTransactionId Id { get; private init; }
+    public string StudentId { get; private init; }
+    public string SubmittedBy { get; private init; }
+    public DateTime SubmittedAt { get; private init; }
+    public ConsentMethod SubmissionMethod { get; private init; }
+    public string SubmissionNotes { get; private init; }
+
+    public IReadOnlyList<Consent> Consents => _consents.ToList();
+
+    public static Result<Transaction> Create(
+        string studentId,
+        string submittedBy,
+        DateTime submittedAt,
+        ConsentMethod submissionMethod,
+        string submissionNotes,
+        Dictionary<ApplicationId, bool> consentResponses)
+    {
+        if (!consentResponses.Any())
+        {
+            return Result.Failure<Transaction>(ConsentErrors.Transaction.NoResponses);
+        }
+
+        Transaction transaction = new Transaction(
+            studentId,
+            submittedBy,
+            submittedAt,
+            submissionMethod,
+            submissionNotes);
+
+        foreach (KeyValuePair<ApplicationId, bool> response in consentResponses)
+        {
+            transaction._consents.Add(Consent.Create(
+                transaction.Id,
+                transaction.StudentId,
+                response.Key,
+                response.Value,
+                transaction.SubmittedBy,
+                transaction.SubmittedAt,
+                transaction.SubmissionMethod,
+                transaction.SubmissionNotes));
+        }
+
+        transaction.RaiseDomainEvent(new ConsentTransactionReceivedDomainEvent(new(), transaction.Id));
+
+        return transaction;
+    }
+}
