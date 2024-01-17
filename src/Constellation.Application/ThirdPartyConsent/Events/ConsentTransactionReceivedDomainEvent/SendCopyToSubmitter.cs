e64b61213244f361e8a444d6beeb2daa3cbe0ec6
Ben <ben@hillsley.dev>
2024-01-17 15:14:31 +1100

    Start to look at generating the Transaction pdf

---
+++ src/Constellation.Application/ThirdPartyConsent/Events/ConsentTransactionReceivedDomainEvent/SendCopyToSubmitter.cs
@@ -0,0 +1,20 @@
+﻿namespace Constellation.Application.ThirdPartyConsent.Events.ConsentTransactionReceivedDomainEvent;
+
+using Abstractions.Messaging;
+using Core.Models.ThirdPartyConsent.Events;
+using System.Threading;
+using System.Threading.Tasks;
+
+internal class SendCopyToSubmitter
+    : IDomainEventHandler<ConsentTransactionReceivedDomainEvent>
+{
+    public SendCopyToSubmitter()
+    {
+        
+    }
+
+    public async Task Handle(ConsentTransactionReceivedDomainEvent notification, CancellationToken cancellationToken)
+    {
+
+    }
+}
---
+++ src/Constellation.Infrastructure.Templates/Views/Documents/ThirdParty/TransactionViewModel.cs
@@ -0,0 +1,37 @@
+﻿namespace Constellation.Infrastructure.Templates.Views.Documents.ThirdParty;
+
+using Constellation.Core.Models.ThirdPartyConsent.Identifiers;
+using Core.Enums;
+using Core.Models.ThirdPartyConsent.Enums;
+using Core.ValueObjects;
+using System;
+using System.Collections.Generic;
+
+public sealed class TransactionViewModel
+{
+    public ConsentTransactionId Id { get; set; }
+    public string SubmittedBy { get; set; }
+    public DateTime SubmittedAt { get; set; }
+    public ConsentMethod SubmissionMethod { get; set; }
+    public string SubmissionNotes { get; set; }
+
+    public string StudentId { get; set; }
+    public Name Name { get; set; }
+    public string School { get; set; }
+    public Grade Grade { get; set; }
+
+    public List<ConsentItem> Consents { get; set; } = new();
+
+    public class ConsentItem
+    {
+        public Core.Models.ThirdPartyConsent.Identifiers.ApplicationId Id { get; set; }
+        public string Name { get; set; }
+        public string Purpose { get; set; }
+        public string[] InformationCollected { get; set; }
+        public string StoredCountry { get; set; }
+        public string[] SharedWith { get; set; }
+        public bool IsDeleted { get; set; }
+
+        public bool ConsentProvided { get; set; }
+    }
+}
