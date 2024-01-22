56b934573530aeb7fe8374af42e6e6272fed9d7f
Ben <ben@hillsley.au>
2024-01-22 22:53:27 +1100

    Finish Consent Application pages, including edit mode

--- src/Constellation.Presentation.Server/Areas/SchoolAdmin/Pages/Consent/TrainingPages.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Constellation.Presentation.Server.Areas.SchoolAdmin.Pages.Consent;
-
-internal sealed class ConsentPages
-{
-    public const string Applications = "Applications";
-    public const string Transactions = "Transactions";
-}
