de92dae5f6d4d2e972ae2084f059c1bdb83dec2a
ben <Benjamin.Hillsley@det.nsw.edu.au>
2024-05-02 15:25:04 +1000

    Clean up DbContext to remove unused DbSets

--- src/Constellation.Application/Interfaces/Repositories/IAppDbContext.cs
+++ src/Constellation.Application/Interfaces/Repositories/IAppDbContext.cs
@@ -3,13 +3,9 @@
 using Constellation.Application.Models;
 using Constellation.Application.Models.Identity;
 using Constellation.Core.Models;
-using Constellation.Core.Models.Faculty;
 using Constellation.Core.Models.Operations;
-using Constellation.Core.Models.SchoolContacts;
-using Constellation.Core.Models.Stocktake;
 using Constellation.Core.Models.Students;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.ChangeTracking;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -23,19 +19,11 @@ public interface IAppDbContext
     DbSet<Device> Devices { get; set; }
     DbSet<JobActivation> JobActivations { get; set; }
     DbSet<MSTeamOperation> MSTeamOperations { get; set; }
-    DbSet<StudentPartialAbsence> PartialAbsences { get; set; }
     DbSet<TimetablePeriod> Periods { get; set; }
     DbSet<AdobeConnectRoom> Rooms { get; set; }
-    DbSet<SchoolContactRole> SchoolContactRoles { get; set; }
-    DbSet<SchoolContact> SchoolContacts { get; set; }
     DbSet<School> Schools { get; set; }
     DbSet<Staff> Staff { get; set; }
     DbSet<Student> Students { get; set; }
-    DbSet<StudentWholeAbsence> WholeAbsences { get; set; }
-    DbSet<StocktakeEvent> StocktakeEvents { get; set; }
-    DbSet<StocktakeSighting> StocktakeSightings { get; set; }
-    DbSet<Faculty> Faculties { get; set; }
     
     Task<int> SaveChangesAsync(CancellationToken cancellationToken);
-    EntityEntry Add(object entity);
 }
--- src/Constellation.Infrastructure/Persistence/ConstellationContext/AppDbContext.cs
+++ src/Constellation.Infrastructure/Persistence/ConstellationContext/AppDbContext.cs
@@ -4,15 +4,11 @@ using Constellation.Application.Interfaces.Repositories;
 using Constellation.Application.Models;
 using Constellation.Application.Models.Identity;
 using Constellation.Core.Models;
-using Constellation.Core.Models.Faculty;
 using Constellation.Core.Models.Operations;
-using Constellation.Core.Models.SchoolContacts;
-using Constellation.Core.Models.Stocktake;
 using Constellation.Core.Models.Students;
 using Constellation.Infrastructure.Persistence.ConstellationContext.ContextExtensions;
 using Duende.IdentityServer.EntityFramework.Options;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Microsoft.Extensions.Options;
 using System.Reflection;
 
@@ -26,8 +22,6 @@ public class AppDbContext : KeyApiAuthorizationDbContext<AppUser, AppRole, Guid>
     public DbSet<MSTeamOperation> MSTeamOperations { get; set; }
     public DbSet<AppAccessToken> AspNetAccessTokens { get; set; }
     public DbSet<School> Schools { get; set; }
-    public DbSet<SchoolContact> SchoolContacts { get; set; }
-    public DbSet<SchoolContactRole> SchoolContactRoles { get; set; }
     public DbSet<Student> Students { get; set; }
     public DbSet<Staff> Staff { get; set; }
     public DbSet<TimetablePeriod> Periods { get; set; }
@@ -35,13 +29,8 @@ public class AppDbContext : KeyApiAuthorizationDbContext<AppUser, AppRole, Guid>
     public DbSet<Device> Devices { get; set; }
     public DbSet<DeviceNotes> DeviceNotes { get; set; }
     public DbSet<DeviceAllocation> DeviceAllocations { get; set; }
-    public DbSet<StudentWholeAbsence> WholeAbsences { get; set; }
-    public DbSet<StudentPartialAbsence> PartialAbsences { get; set; }
     public DbSet<CanvasOperation> CanvasOperations { get; set; }
     public DbSet<JobActivation> JobActivations { get; set; }
-    public DbSet<StocktakeEvent> StocktakeEvents { get; set; }
-    public DbSet<StocktakeSighting> StocktakeSightings { get; set; }
-    public DbSet<Faculty> Faculties { get; set; }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
@@ -50,6 +39,4 @@ public class AppDbContext : KeyApiAuthorizationDbContext<AppUser, AppRole, Guid>
 
         base.OnModelCreating(builder);
     }
-
-    public override EntityEntry Add(object entity) => base.Add(entity);
 }
