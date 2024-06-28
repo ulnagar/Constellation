0b43d8f4e8303c4c5a56407c856fa592193417e3
ben <Benjamin.Hillsley@det.nsw.edu.au>
2024-06-28 10:46:51 +1000

    Remove redundant files

--- src/Constellation.Presentation.Server/BaseModels/BaseViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Constellation.Presentation.Server.BaseModels;
-
-using Application.Common.PresentationModels;
-
-public class BaseViewModel : IBaseModel
-{
-    // Application Settings
-    public ErrorDisplay Error { get; set; }
-}
