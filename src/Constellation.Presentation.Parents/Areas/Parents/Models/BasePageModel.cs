baa1798d4f403e765b7cb883ce339c43f36bb616
Ben <ben@hillsley.au>
2024-07-07 21:41:24 +1000

    Add Parent Portal project

---
+++ src/Constellation.Presentation.Parents/Areas/Parents/Models/BasePageModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Constellation.Presentation.Parents.Areas.Parents.Models;
+
+using Constellation.Application.Common.PresentationModels;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+public class BasePageModel : PageModel, IBaseModel
+{
+    public ModalContent? ModalContent { get; set; }
+}
---
+++ src/Constellation.Presentation.Server/wwwroot/js/parents.js
@@ -0,0 +1,13 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+function showLoader(delay = 0) {
+    $('.spinner-overlay').removeClass('d-none');
+
+    if (delay > 0) {
+        setTimeout(function () {
+            $('.spinner-overlay').addClass('d-none');
+        }, delay);
+    }
+}
