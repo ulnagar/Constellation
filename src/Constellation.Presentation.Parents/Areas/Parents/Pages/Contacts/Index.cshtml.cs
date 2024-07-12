b7cae3ba61e8d4d7f4ba1987afa69849d4ebba39
Ben <ben@hillsley.au>
2024-07-12 12:02:12 +1000

    Implement Contacts page from old Parents portal

---
+++ src/Constellation.Presentation.Parents/Areas/Parents/Pages/Contacts/Index.cshtml.cs
@@ -0,0 +1,89 @@
+namespace Constellation.Presentation.Parents.Areas.Parents.Pages.Contacts;
+
+using Application.Common.PresentationModels;
+using Application.Models.Auth;
+using Constellation.Application.Awards.GetSummaryForStudent;
+using Constellation.Application.Contacts.GetContactListForParentPortal;
+using Constellation.Application.Students.GetStudentsByParentEmail;
+using Core.Abstractions.Services;
+using Core.Shared;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Models;
+using Serilog;
+
+[Authorize(Policy = AuthPolicies.IsParent)]
+public class IndexModel : BasePageModel
+{
+    private readonly ISender _mediator;
+    private readonly LinkGenerator _linkGenerator;
+    private readonly ICurrentUserService _currentUserService;
+    private readonly ILogger _logger;
+
+    public IndexModel(
+        ISender mediator,
+        LinkGenerator linkGenerator,
+        ICurrentUserService currentUserService,
+        ILogger logger)
+    {
+        _mediator = mediator;
+        _linkGenerator = linkGenerator;
+        _currentUserService = currentUserService;
+        _logger = logger.ForContext<IndexModel>();
+    }
+
+    [ViewData] public string ActivePage => Models.ActivePage.Contacts;
+
+    [BindProperty(SupportsGet = true)]
+    public string StudentId { get; set; } = string.Empty;
+
+    public StudentResponse? SelectedStudent { get; set; }
+
+    public List<StudentResponse> Students { get; set; } = new();
+
+    public List<StudentSupportContactResponse> Contacts { get; set; } = new();
+
+    public async Task OnGet() => await PreparePage();
+
+    public async Task PreparePage()
+    {
+        _logger.Information("Requested to retrieve student list by user {user}", _currentUserService.UserName);
+
+        Result<List<StudentResponse>> studentsRequest = await _mediator.Send(new GetStudentsByParentEmailQuery(_currentUserService.EmailAddress));
+
+        if (studentsRequest.IsFailure)
+        {
+            ModalContent = new ErrorDisplay(studentsRequest.Error);
+
+            return;
+        }
+
+        Students = studentsRequest.Value
+            .OrderBy(student => student.CurrentGrade)
+            .ThenBy(student => student.LastName)
+            .ThenBy(student => student.FirstName)
+            .ToList();
+
+        if (Students.Count == 1)
+            StudentId = Students.First().StudentId;
+
+        if (!string.IsNullOrWhiteSpace(StudentId))
+        {
+            _logger.Information("Requested to retrieve contacts by user {user} for student {student}", _currentUserService.UserName, StudentId);
+
+            Result<List<StudentSupportContactResponse>> contactsRequest = await _mediator.Send(new GetContactListForParentPortalQuery(StudentId));
+            
+            if (contactsRequest.IsFailure)
+            {
+                ModalContent = new ErrorDisplay(contactsRequest.Error);
+
+                return;
+            }
+
+            Contacts = contactsRequest.Value;
+            SelectedStudent = Students.FirstOrDefault(entry => entry.StudentId == StudentId);
+        }
+    }
+}
