cdbc500cdd0b9ba247f9fd18e0c12b1745b9f93c
Ben <ben@hillsley.au>
2024-07-17 15:38:18 +1000

    Remove old Parent and Schools portals from git

--- src/Constellation.Portal.Schools.Client/Shared/Models/AbsencesExplainFormModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Constellation.Portal.Schools.Client.Shared.Models;
-
-public sealed class AbsencesExplainFormModel
-{
-    public Guid AbsenceId { get; set; }
-    public string Comment { get; set; }
-    public string Username { get; set; }
-}
