e8d371921d59bb726faa7ad3f6c52e472d697207
ben <ben@hillsley.au>
2024-10-29 09:56:37 +1100

    Add alert on Parent Portal dashboard when there are applications pending consent response. Exclude for non-residential parents.

---
+++ src/Constellation.Application/ThirdPartyConsent/DoesStudentHaveRequiredApplicationWithoutConsent/DoesStudentHaveRequiredApplicationWithoutConsentQuery.cs
@@ -0,0 +1,8 @@
+﻿namespace Constellation.Application.ThirdPartyConsent.DoesStudentHaveRequiredApplicationWithoutConsent;
+
+using Abstractions.Messaging;
+using Core.Models.Students.Identifiers;
+
+public sealed record DoesStudentHaveRequiredApplicationWithoutConsentQuery(
+    StudentId StudentId)
+    : IQuery<bool>;
