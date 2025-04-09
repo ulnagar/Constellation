c1641bf0544c3e6cf07d1be47160f5aa1c62d9bb
Ben Hillsley <Benjamin.Hillsley@det.nsw.edu.au>
2025-04-09 15:08:21 +1000

    Fix model binding error for SchoolTerm and SchoolWeek enums

--- src/Constellation.Presentation.Staff/Areas/Staff/Pages/Shared/Components/HistoricalAttendanceReport/HistoricalAttendanceReportSelection.cs
+++ src/Constellation.Presentation.Staff/Areas/Staff/Pages/Shared/Components/HistoricalAttendanceReport/HistoricalAttendanceReportSelection.cs
@@ -1,6 +1,8 @@
 ﻿namespace Constellation.Presentation.Staff.Areas.Staff.Pages.Shared.Components.HistoricalAttendanceReport;
 
 using Constellation.Core.Enums;
+using Microsoft.AspNetCore.Mvc;
+using Presentation.Shared.Helpers.ModelBinders;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,9 +12,13 @@ using System.Threading.Tasks;
 public sealed class HistoricalAttendanceReportSelection
 {
     public string Year { get; set; }
+    [ModelBinder(typeof(BaseFromValueBinder))]
     public SchoolTerm StartTerm { get; set; }
+    [ModelBinder(typeof(BaseFromValueBinder))]
     public SchoolWeek StartWeek { get; set; }
+    [ModelBinder(typeof(BaseFromValueBinder))]
     public SchoolTerm EndTerm { get; set; }
+    [ModelBinder(typeof(BaseFromValueBinder))]
     public SchoolWeek EndWeek { get; set; }
 
     public List<string> Years { get; set; }
