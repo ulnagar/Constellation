8dfd531244462478a4690b30a0357791bbacbc02
Ben Hillsley <Benjamin.Hillsley@det.nsw.edu.au>
2025-05-08 09:14:50 +1000

    Implement EdvalTeachersUpdatedIntegrationEvent handler

--- src/Constellation.Application/Domains/Edval/Events/EdvalTeachersUpdated/CalculateDifferences.cs
+++ src/Constellation.Application/Domains/Edval/Events/EdvalTeachersUpdated/CalculateDifferences.cs
@@ -1,18 +1,87 @@
 ﻿namespace Constellation.Application.Domains.Edval.Events.EdvalTeachersUpdated;
 
 using Abstractions.Messaging;
+using Constellation.Core.Models.Edval.Enums;
+using Constellation.Core.Models.Students;
+using Core.Models;
+using Core.Models.Edval;
 using Core.Models.Edval.Events;
+using Core.Models.StaffMembers.Repositories;
+using Interfaces.Repositories;
+using Repositories;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
 internal sealed class CalculateDifferences : IIntegrationEventHandler<EdvalTeachersUpdatedIntegrationEvent>
 {
-    public CalculateDifferences()
+    private readonly IEdvalRepository _edvalRepository;
+    private readonly IStaffRepository _staffRepository;
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly ILogger _logger;
+
+    public CalculateDifferences(
+        IEdvalRepository edvalRepository,
+        IStaffRepository staffRepository,
+        IUnitOfWork unitOfWork,
+        ILogger logger)
     {
-        
+        _edvalRepository = edvalRepository;
+        _staffRepository = staffRepository;
+        _unitOfWork = unitOfWork;
+        _logger = logger
+            .ForContext<EdvalTeachersUpdatedIntegrationEvent>();
     }
     public async Task Handle(EdvalTeachersUpdatedIntegrationEvent notification, CancellationToken cancellationToken)
     {
-        
+        List<Staff> existingStaff = await _staffRepository.GetAllActive(cancellationToken);
+
+        List<EdvalTeacher> edvalTeachers = await _edvalRepository.GetTeachers(cancellationToken);
+
+        foreach (EdvalTeacher teacher in edvalTeachers)
+        {
+            Staff staffMember = existingStaff.FirstOrDefault(member =>
+                member.FirstName.Equals(teacher.FirstName, StringComparison.OrdinalIgnoreCase) &&
+                member.LastName.Equals(teacher.LastName, StringComparison.OrdinalIgnoreCase));
+
+            if (staffMember is null)
+            {
+                // Additional staff member in Edval
+                _edvalRepository.Insert(new Difference()
+                {
+                    Type = EdvalDifferenceType.EdvalTeacher,
+                    Description = $"{teacher.FirstName} {teacher.LastName} is not present in Constellation"
+                });
+                continue;
+            }
+
+            if (!staffMember.EmailAddress.Equals(teacher.EmailAddress, StringComparison.OrdinalIgnoreCase))
+            {
+                _edvalRepository.Insert(new Difference()
+                {
+                    Type = EdvalDifferenceType.EdvalTeacher,
+                    Description = $"{staffMember.FirstName} {staffMember.LastName} has a different Email Address ({teacher.EmailAddress}) in Edval"
+                });
+            }
+        }
+
+        foreach (Staff staffMember in existingStaff)
+        {
+            if (!edvalTeachers.All(teacher => 
+                    teacher.FirstName.Equals(staffMember.FirstName, StringComparison.OrdinalIgnoreCase) &&
+                    teacher.LastName.Equals(staffMember.LastName, StringComparison.OrdinalIgnoreCase)))
+            {
+                _edvalRepository.Insert(new Difference()
+                {
+                    Type = EdvalDifferenceType.EdvalTeacher,
+                    Description = $"{staffMember.FirstName} {staffMember.LastName} is not present in Edval"
+                });
+            }
+        }
+
+        await _unitOfWork.CompleteAsync(cancellationToken);
     }
 }
