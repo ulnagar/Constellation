989e5894accf769f54e463fe73f5382c1c21db74
Ben Hillsley <Benjamin.Hillsley@det.nsw.edu.au>
2025-06-06 09:28:48 +1000

    Fix dispaly  issues for non-styled (default) recent updates

--- src/Constellation.Presentation.Staff/Areas/Staff/Pages/Shared/Components/ShowDashboardUpdates/ShowDashboardUpdatesViewComponentModel.cs
+++ src/Constellation.Presentation.Staff/Areas/Staff/Pages/Shared/Components/ShowDashboardUpdates/ShowDashboardUpdatesViewComponentModel.cs
@@ -9,6 +9,6 @@ public sealed class ShowDashboardUpdatesViewComponentModel
         public DateTime Datestamp { get; set; }
         public string Title { get; set; } = string.Empty;
         public string Body { get; set; } = string.Empty;
-        public string Type { get; set; } = "dark";
+        public string Type { get; set; } = string.Empty;
     }
 }
