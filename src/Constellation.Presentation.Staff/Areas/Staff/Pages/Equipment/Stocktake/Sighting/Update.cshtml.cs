331cd861f021b3d5834b44a4842828e952d3be78
Ben Hillsley <Benjamin.Hillsley@det.nsw.edu.au>
2025-06-25 15:56:38 +1000

    Convert the Staff Stocktake sighting page to use Jumbotrons instead of modals.

---
+++ src/Constellation.Presentation.Staff/Areas/Staff/Pages/Equipment/Stocktake/Sighting/Update.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Constellation.Presentation.Staff.Areas.Staff.Pages.Equipment.Stocktake.Sighting
+{
+    public class UpdateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
