6d7419fc644cd315601589db575ac34c03b84846
Ben Hillsley <Benjamin.Hillsley@det.nsw.edu.au>
2025-07-21 15:30:49 +1000

    Fix bug in Constellation Assignment update where multiple courses were linked to the same course. Fix bug where Operations would not be able to be generated for staff.

--- src/Constellation.Core/Models/Assignments/Services/IAssignmentService.cs
+++ src/Constellation.Core/Models/Assignments/Services/IAssignmentService.cs
@@ -2,10 +2,11 @@
 
 using Canvas.Models;
 using Shared;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
 public interface IAssignmentService
 {
-    Task<Result> UploadSubmissionToCanvas(CanvasAssignment assignment, CanvasAssignmentSubmission submission, CanvasCourseCode canvasCourseId, CancellationToken cancellationToken = default);
+    Task<Result> UploadSubmissionToCanvas(CanvasAssignment assignment, CanvasAssignmentSubmission submission, List<CanvasCourseCode> canvasCourseId, CancellationToken cancellationToken = default);
 }
