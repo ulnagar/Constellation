0b539beb376934e3c9f09ee7ff7dc20640daab7e
Ben Hillsley <Benjamin.Hillsley@det.nsw.edu.au>
2025-09-01 11:50:59 +1000

    Update staff dashboard to include stocktake stats

---
+++ src/Constellation.Application/Domains/AssetManagement/Stocktake/Queries/CountStocktakeItemsOutstanding/CountStocktakeItemsOutstandingQuery.cs
@@ -0,0 +1,6 @@
+﻿namespace Constellation.Application.Domains.AssetManagement.Stocktake.Queries.CountStocktakeItemsOutstanding;
+
+using Abstractions.Messaging;
+
+public sealed record CountStocktakeItemsOutstandingQuery()
+    : IQuery<double>;
---
+++ src/Constellation.Application/Domains/AssetManagement/Stocktake/Queries/CountStocktakeItemsOutstanding/CountStocktakeItemsOutstandingQueryHandler.cs
@@ -0,0 +1,47 @@
+﻿namespace Constellation.Application.Domains.AssetManagement.Stocktake.Queries.CountStocktakeItemsOutstanding;
+
+using Abstractions.Messaging;
+using Constellation.Core.Models.Stocktake;
+using Core.Models.Assets;
+using Core.Models.Assets.Repositories;
+using Core.Models.Stocktake.Repositories;
+using Core.Shared;
+using Serilog;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+internal sealed class CountStocktakeItemsOutstandingQueryHandler
+: IQueryHandler<CountStocktakeItemsOutstandingQuery, double>
+{
+    private readonly IAssetRepository _assetRepository;
+    private readonly IStocktakeRepository _stocktakeRepository;
+    private readonly ILogger _logger;
+
+    public CountStocktakeItemsOutstandingQueryHandler(
+        IAssetRepository assetRepository,
+        IStocktakeRepository stocktakeRepository,
+        ILogger logger)
+    {
+        _assetRepository = assetRepository;
+        _stocktakeRepository = stocktakeRepository;
+        _logger = logger;
+    }
+
+    public async Task<Result<double>> Handle(CountStocktakeItemsOutstandingQuery request, CancellationToken cancellationToken)
+    {
+        List<Asset> activeAssets = await _assetRepository.GetAllActive(cancellationToken);
+
+        List<StocktakeEvent> currentEvents = await _stocktakeRepository.GetCurrentEvents(cancellationToken);
+
+        if (currentEvents.Count == 0)
+            return Result.Failure<double>(Error.NullValue);
+
+        double sightedDeviceCount = currentEvents
+            .SelectMany(item => item.Sightings)
+            .Count(sighting => !sighting.IsCancelled);
+
+        return ((activeAssets.Count - sightedDeviceCount) / activeAssets.Count) * 100;
+    }
+}
