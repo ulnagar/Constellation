dffce1cc4bbfad1579610b7b2acd0bbba8dfad4c
ulnagar <ulnagar@users.noreply.github.com>
2025-12-07 00:39:20 +0000

    Expand the AddPhoneNumberToStaffMember command handler

--- src/Constellation.Application/Domains/StaffMembers/Commands/AddPhoneNumberToStaffMember/AddPhoneNumberToStaffMemberCommand.cs
+++ src/Constellation.Application/Domains/StaffMembers/Commands/AddPhoneNumberToStaffMember/AddPhoneNumberToStaffMemberCommand.cs
@@ -1,5 +1,8 @@
-namespace Constellation.Application.Domains.StaffMember.Commands.AddPhoneNumberToStaffMember
+namespace Constellation.Application.Domains.StaffMember.Commands.AddPhoneNumberToStaffMember;
 
+using Constellation.Core.Models.StaffMembers.Identifiers;
+using Constellation.Core.ValueObjects;
+using Constellation.Application.Abstractions.Messaging;
 public sealed record AddPhoneNumberToStaffMemberCommand(
     StaffId StaffId,
     PhoneNumber PhoneNumber)
--- src/Constellation.Application/Domains/StaffMembers/Commands/AddPhoneNumberToStaffMember/AddPhoneNumberToStaffMemberCommandHandler.cs
+++ src/Constellation.Application/Domains/StaffMembers/Commands/AddPhoneNumberToStaffMember/AddPhoneNumberToStaffMemberCommandHandler.cs
@@ -1,9 +1,18 @@
-namespace Constellation.Application.Domains.StaffMember.Commands.AddPhoneNumberToStaffMember
+namespace Constellation.Application.Domains.StaffMember.Commands.AddPhoneNumberToStaffMember;
+
+using Constellation.Application.Abstractions.Messaging;
+using Constellation.Core.Models.StaffMembers;
+using Constellation.Core.Models.StaffMembers.Repositories;
+using Constellation.Application.Interfaces.Repositories;
+using Serilog;
+using System.Threading.Tasks;
+using Constellation.Core.Shared;
+using System.Threading;
+using Constellation.Core.Models.StaffMembers.Errors;
 
 internal sealed class AddPhoneNumberToStaffMemberCommandHandler
     : ICommandHandler<AddPhoneNumberToStaffMemberCommand>
 {
-
     private IStaffRepository _staffRepository {get;set;}
     private IUnitOfWork _unitOfWork {get;set;}
     private ILogger _logger {get;set;}
@@ -18,5 +27,21 @@ internal sealed class AddPhoneNumberToStaffMemberCommandHandler
         _logger = logger
             .ForContext<AddPhoneNumberToStaffMemberCommand>();     
     }
-        
+
+    public async Task<Result> Handle(AddPhoneNumberToStaffMemberCommand request, CancellationToken cancellationToken)
+    {
+        StaffMember staffMember = await _staffRepository.GetById(request.StaffId, cancellationToken);
+
+        if (staffMember is null)
+        {
+            return Result.Failure(StaffMemberErrors.NotFound(request.StaffId));
+        }
+
+        staffMember.AddPhoneNumber(request.PhoneNumber);
+
+        await _unitOfWork.CompleteAsync(cancellationToken);
+
+        return Result.Success();
+    }
+
 }
