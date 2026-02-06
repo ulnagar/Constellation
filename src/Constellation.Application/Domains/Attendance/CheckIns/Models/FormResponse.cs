e6d1759668cdac982c9bf52847a6c2e890a62927
Ben Hillsley <Benjamin.Hillsley@det.nsw.edu.au>
2026-02-06 11:13:16 +1100

    Update FormResponse to handle string timestamps in US format.

--- src/Constellation.Application/Domains/Attendance/CheckIns/Models/FormResponse.cs
+++ src/Constellation.Application/Domains/Attendance/CheckIns/Models/FormResponse.cs
@@ -1,5 +1,6 @@
 ﻿namespace Constellation.Application.Domains.Attendance.CheckIns.Models;
 
+using System.Globalization;
 using System.Text.Json.Serialization;
 
 public sealed class FormResponse
@@ -11,5 +12,34 @@ public sealed class FormResponse
     public string? Subject { get; set; }
     public GroupOption Group { get; set; }
 
-    public DateTime? Submitted => string.IsNullOrWhiteSpace(StringDate) ? null : DateTime.Parse(StringDate).ToLocalTime();
+    public DateTime? Submitted => GetSubmittedDateTime();
+
+    private DateTime? GetSubmittedDateTime()
+    {
+        if (string.IsNullOrWhiteSpace(StringDate))
+            return null;
+
+        string[] parts = StringDate.Split(' ');
+
+        string date = parts[0];
+
+        string[] dateParts = date.Split('/');
+        int day = Convert.ToInt32(dateParts[0], CultureInfo.InvariantCulture);
+        int month = Convert.ToInt32(dateParts[1], CultureInfo.InvariantCulture);
+        int year = Convert.ToInt32(dateParts[2], CultureInfo.InvariantCulture);
+
+        string[] timeParts = parts[1].Split(':');
+        int hours = Convert.ToInt32(timeParts[0], CultureInfo.InvariantCulture);
+        int minutes = Convert.ToInt32(timeParts[1], CultureInfo.InvariantCulture);
+        int seconds = Convert.ToInt32(timeParts[2], CultureInfo.InvariantCulture);
+
+        hours = parts[2] == "AM" ? hours : hours + 12;
+
+        DateTime returnDate =
+            (DateTime.UtcNow.Day == day)
+                ? new DateTime(year, month, day, hours, minutes, seconds, DateTimeKind.Utc).ToLocalTime()
+                : new DateTime(year, day, month, hours, minutes, seconds, DateTimeKind.Utc).ToLocalTime();
+
+        return returnDate;
+    }
 }
