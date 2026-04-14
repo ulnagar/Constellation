49d2acde6dfac2d3ac7c16ddebde34dbda1467d1
Ben Hillsley <Benjamin.Hillsley@det.nsw.edu.au>
2026-04-14 15:41:23 +1000

    Expand the IAssessmentRepository to include methods for retrieving lists of assessments.

--- src/Constellation.Core/Models/Assessments/Repositories/IAssessmentRepository.cs
+++ src/Constellation.Core/Models/Assessments/Repositories/IAssessmentRepository.cs
@@ -1,8 +1,15 @@
 ﻿namespace Constellation.Core.Models.Assessments.Repositories;
 
 using Identifiers;
+using Students.Identifiers;
 
 public interface IAssessmentRepository
 {
     Task<Assessment?> GetById(AssessmentId id, CancellationToken cancellationToken = default);
+
+    Task<List<Assessment>> GetCurrentAssessments(CancellationToken cancellationToken = default);
+    Task<List<Assessment>> GetForStudent(StudentId studentId, CancellationToken cancellationToken = default);
+    Task<List<Assessment>> GetCurrentForStudent(StudentId studentId, CancellationToken cancellationToken = default);
+
+    void Insert(Assessment assessment);
 }
--- src/Constellation.Infrastructure/Persistence/ConstellationContext/Repositories/AssessmentRepository.cs
+++ src/Constellation.Infrastructure/Persistence/ConstellationContext/Repositories/AssessmentRepository.cs
@@ -1,5 +1,6 @@
 ﻿namespace Constellation.Infrastructure.Persistence.ConstellationContext.Repositories;
 
+using Constellation.Core.Models.Students.Identifiers;
 using Core.Models.Assessments;
 using Core.Models.Assessments.Identifiers;
 using Core.Models.Assessments.Repositories;
@@ -21,4 +22,42 @@ internal sealed class AssessmentRepository : IAssessmentRepository
         await _context
             .Set<Assessment>()
             .FirstOrDefaultAsync(assessment => assessment.Id == id, cancellationToken);
+
+    public async Task<List<Assessment>> GetCurrentAssessments(
+        CancellationToken cancellationToken = default)
+    {
+        var now = DateTimeOffset.UtcNow;
+
+        return await _context
+            .Set<Assessment>()
+            .Where(a => 
+                a.AvailableFrom <= now 
+                && a.AvailableTo >= now)
+            .ToListAsync(cancellationToken);
+    }
+
+    public async Task<List<Assessment>> GetForStudent(
+        StudentId studentId,
+        CancellationToken cancellationToken = default) =>
+        await _context
+            .Set<Assessment>()
+            .Where(a => a.Students.Any(s => s.StudentId == studentId))
+            .ToListAsync(cancellationToken);
+
+    public async Task<List<Assessment>> GetCurrentForStudent(
+        StudentId studentId,
+        CancellationToken cancellationToken = default)
+    {
+        DateTimeOffset now = DateTimeOffset.UtcNow;
+
+        return await _context
+            .Set<Assessment>()
+            .Where(a =>
+                a.AvailableFrom <= now
+                && a.AvailableTo >= now
+                && a.Students.Any(s => s.StudentId == studentId))
+            .ToListAsync(cancellationToken);
+    }
+
+    public void Insert(Assessment assessment) => _context.Set<Assessment>().Add(assessment);
 }
