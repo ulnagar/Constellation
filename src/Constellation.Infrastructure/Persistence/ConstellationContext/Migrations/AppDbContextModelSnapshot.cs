6c00caaa86c528dc93a634bb8334d68c0d485cec
Ben Hillsley <Benjamin.Hillsley@det.nsw.edu.au>
2026-04-16 13:24:23 +1000

    Remove transitive package with security vulnerability.

--- src/Constellation.Infrastructure/Persistence/ConstellationContext/Migrations/AppDbContextModelSnapshot.cs
+++ src/Constellation.Infrastructure/Persistence/ConstellationContext/Migrations/AppDbContextModelSnapshot.cs
@@ -162,7 +162,7 @@ namespace Constellation.Infrastructure.Persistence.ConstellationContext.Migratio
                     b.HasIndex("JobName")
                         .IsUnique();
 
-                    b.ToTable("JobActivations");
+                    b.ToTable("JobActivations", (string)null);
                 });
 
             modelBuilder.Entity("Constellation.Core.Models.Absences.Absence", b =>
@@ -2042,7 +2042,7 @@ namespace Constellation.Infrastructure.Persistence.ConstellationContext.Migratio
 
                     b.HasIndex("SerialNumber");
 
-                    b.ToTable("DeviceNotes");
+                    b.ToTable("DeviceNotes", (string)null);
                 });
 
             modelBuilder.Entity("Constellation.Core.Models.Edval.Difference", b =>
@@ -7313,7 +7313,7 @@ namespace Constellation.Infrastructure.Persistence.ConstellationContext.Migratio
 
                             b1.HasKey("SendEmailActionId");
 
-                            b1.ToTable("WorkFlows_Actions");
+                            b1.ToTable("WorkFlows_Actions", (string)null);
 
                             b1.WithOwner()
                                 .HasForeignKey("SendEmailActionId");
